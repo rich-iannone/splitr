fd6a339275f7e5bf1401a9f545426a3fd67c824b
rich-iannone <riannone@me.com>
2013-12-05 14:53:19 -0800

    Include variables for system paths

--- SplitR.R
+++ SplitR.R
@@ -2,4 +2,9 @@
 # Define package requirements
 require(lubridate)
 
+# Identify file system paths (UNIX/Linux)
+path_met_files <- "~/Downloads/Hysplit4-OSX/gdas1/"
+path_output_files <- "~/Downloads/Hysplit4-OSX/output_trajectory/"
+path_wd <- "~/Downloads/Hysplit4-OSX/working/"
+path_executable <- "~/Downloads/Hysplit4-OSX/exec/hyts_std"
 }
