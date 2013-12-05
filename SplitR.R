2fac67f17980410fb9348e9fc38397658782ee1b
rich-iannone <riannone@me.com>
2013-12-05 14:54:21 -0800

    Include objects for non-varying conditions
    These parameters go into the main CONTROL file and
    they are non-temporal.

--- SplitR.R
+++ SplitR.R
@@ -7,4 +7,14 @@ path_met_files <- "~/Downloads/Hysplit4-OSX/gdas1/"
 path_output_files <- "~/Downloads/Hysplit4-OSX/output_trajectory/"
 path_wd <- "~/Downloads/Hysplit4-OSX/working/"
 path_executable <- "~/Downloads/Hysplit4-OSX/exec/hyts_std"
+
+# Non-varying model parameters      
+no_starting_locations <- 1
+start_lat_deg <- 50.108
+start_long_deg <- -122.942
+start_height_m_ASL <- 2200.0
+backtrajectory <- TRUE
+simulation_duration_h <- 48
+vertical_motion_option <- 0
+top_of_model_domain_m <- 20000
 }
