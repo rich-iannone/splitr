bd616694412ca905225c8f94db5209ce185d2efa
rich-iannone <riannone@me.com>
2013-12-06 15:07:13 -0800

    Change default test for met files

--- SplitR.R
+++ SplitR.R
@@ -4,7 +4,7 @@
 require(lubridate)
 
 # Identify file system paths (UNIX/Linux)
-path_met_files <- "~/Downloads/Hysplit4-OSX/gdas1/"
+path_met_files <- "~/Downloads/Hysplit4-OSX/met/"
 path_output_files <- "~/Downloads/Hysplit4-OSX/output_trajectory/"
 path_wd <- "~/Downloads/Hysplit4-OSX/working/"
 path_executable <- "~/Downloads/Hysplit4-OSX/exec/hyts_std"
