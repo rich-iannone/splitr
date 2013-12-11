ba436be04ffd118f6d93035c0e0378f92a23e80f
rich-iannone <riannone@me.com>
2013-12-10 23:08:36 -0800

    Add statement to generate file list for function

--- R/trajectory.read.R
+++ R/trajectory.read.R
@@ -3,4 +3,6 @@
 # Get file path for trajectory files
 path_trajectory_files <- "~/Downloads/Hysplit4-OSX/output_trajectory/"
 
+# Generate file list
+trajectory_file_list <- list.files(path = path_trajectory_files)
 }
