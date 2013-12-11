869a96c02bb32f52cb0fdd3ff2a2ac12fe4df5fd
rich-iannone <riannone@me.com>
2013-12-10 23:09:04 -0800

    Init an empty df with 12 named columns

--- R/trajectory.read.R
+++ R/trajectory.read.R
@@ -5,4 +5,10 @@ path_trajectory_files <- "~/Downloads/Hysplit4-OSX/output_trajectory/"
 
 # Generate file list
 trajectory_file_list <- list.files(path = path_trajectory_files)
+
+# Initialize empty data frame with 12 named columns
+traj.df <- setNames(data.frame(mat.or.vec(nr = 0, nc = 12)),
+                    nm = c("receptor", "year", "month", "day",
+                           "hour", "hour.inc", "lat", "lon", 
+                           "height", "pressure", "date2", "date"))
 }
