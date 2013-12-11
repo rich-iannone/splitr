f89dd72e3efe6a347a4611f349e0b9c6df1f2c9b
rich-iannone <riannone@me.com>
2013-12-10 23:09:42 -0800

    Init loop with all trajectory files in specified dir

--- R/trajectory.read.R
+++ R/trajectory.read.R
@@ -11,4 +11,8 @@ traj.df <- setNames(data.frame(mat.or.vec(nr = 0, nc = 12)),
                     nm = c("receptor", "year", "month", "day",
                            "hour", "hour.inc", "lat", "lon", 
                            "height", "pressure", "date2", "date"))
+
+# Make loop with all trajectory files
+for (i in 1:length(trajectory_file_list)) {
+  
 }
