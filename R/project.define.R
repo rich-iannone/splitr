62c2e32c4c613d60918aad1006e55bb4052efd75
rich-iannone <riannone@me.com>
2013-12-12 07:20:20 -0800

    Define function ‘project.define'

--- R/project.define.R
+++ R/project.define.R
@@ -1,1 +1,13 @@
-project.define <- function( ){ }
+project.define <- function(project_name,
+                           install_folders = FALSE,
+                           install_in_documents = TRUE,
+                           other_install_path){
+  
+  # This function allows for the creation of a new project folder
+  # It also allows for the setting of global options that apply to all projects
+  
+  install_folders = FALSE
+  install_in_documents = TRUE
+  #other_install_path = ""
+  
+}
