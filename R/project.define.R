3ca4dd3d8565168cc1ad2d578f920a4f9ff7560c
rich-iannone <riannone@me.com>
2013-12-13 07:51:55 -0800

    Refactor SplitR folder tree existance/creation

--- R/project.define.R
+++ R/project.define.R
@@ -25,24 +25,25 @@ project.define <- function(project_name,
     documents_folder_path <- paste("~", "/Documents", sep = '')
   }
   
-  # Check existence of SplitR directory and create if doesn't exist
+  # Check existence of SplitR folder
   SplitR_dir_exists <- ifelse(length(list.dirs(path = paste(documents_folder_path,
                                                             "/SplitR", sep = ''),
                                                full.names = TRUE, recursive = FALSE)) > 0, 
                               TRUE, FALSE)
   
-  if(SplitR_dir_exists == FALSE) dir.create(file.path(paste(documents_folder_path,
-                                                            "/SplitR", sep = '')),
-                                            showWarnings = FALSE)
+  # If it the SplitR folder doesn't exist, crete the folder tree in ~/Documents
+  if(SplitR_dir_exists == FALSE) {
+    dir.create(file.path(paste(documents_folder_path,
+                               "/SplitR", sep = '')),
+               showWarnings = FALSE)
   
   # Set new SplitR directory as the working directory
-  setwd(file.path(paste(documents_folder_path, "/SplitR", sep = '')))
+  # setwd(file.path(paste(documents_folder_path, "/SplitR", sep = '')))
   
   # Set path of SplitR directory
   SplitR_path <- file.path(paste(documents_folder_path, "/SplitR", sep = ''))
   
-  # If the SplitR directory didn't exist, create the other subfolders (Exec, Met, Projects)
-  if(SplitR_dir_exists == FALSE){
+  # Subfolder creation (Exec, Met, Projects)
     dir.create(file.path(paste(documents_folder_path, "/SplitR/Exec", sep = '')),
                showWarnings = FALSE)
     dir.create(file.path(paste(documents_folder_path, "/SplitR/Met", sep = '')),
