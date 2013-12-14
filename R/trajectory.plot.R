3639d943bf59d7ca698d783a34d5272a2a15297a
rich-iannone <riannone@me.com>
2013-12-13 16:32:09 -0800

    Define arguments for trajectory.plot function

--- R/trajectory.plot.R
+++ R/trajectory.plot.R
@@ -1,3 +1,19 @@
-trajectory.plot <- function(traj_df){
+trajectory.plot <- function(traj_df = "traj.df",
+                            lon = "lon",
+                            lat = "lat",
+                            pollutant = "height", 
+                            type = "default",
+                            smooth = FALSE,
+                            statistic = "mean",
+                            percentile = 90, 
+                            map = TRUE,
+                            lon.inc = 1,
+                            lat.inc = 1,
+                            min.bin = 1,
+                            group = NA, 
+                            map.fill = TRUE,
+                            map.res = "default",
+                            map.cols = "grey40", 
+                            map.alpha = 0.4){
   
 }
