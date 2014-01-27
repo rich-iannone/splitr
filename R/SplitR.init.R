ade77274df1926b15e7965a41448f79e136fba60
rich-iannone <riannone@me.com>
2014-01-26 16:48:05 -0800

    Add test data and add closing brace to function

--- R/SplitR.init.R
+++ R/SplitR.init.R
@@ -2,3 +2,10 @@ SplitR.init <- function(install_folders = TRUE,
                         install_in_documents = TRUE,
                         other_install_path){
   
+  
+  # Test data
+  install_folders = TRUE
+  install_in_documents = TRUE
+  #other_install_path = ""
+  # Close function
+}
