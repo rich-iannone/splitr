b2550a8214aa46f9b1a619b41cf4d25ba9aa7071
rich-iannone <riannone@me.com>
2014-01-28 00:51:38 -0800

    Use ‘switch’ statement to number the headings in order

--- R/SplitR.info.R
+++ R/SplitR.info.R
@@ -9,4 +9,11 @@ SplitR.info <- function(mode = "add",
 #   heading <- "paths"
 #   string <- "test"
   
+  heading_order <- switch(heading,
+                          paths = 1,
+                          platform = 2,
+                          executables = 3,
+                          met = 4,
+                          log = 5)
+  
 }
