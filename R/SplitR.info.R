e3577ac785ed228d6b739f645cb65b7696a7e3cf
rich-iannone <riannone@me.com>
2014-01-28 00:52:30 -0800

    Get path of SplitR folder

--- R/SplitR.info.R
+++ R/SplitR.info.R
@@ -28,4 +28,62 @@ SplitR.info <- function(mode = "add",
     ifelse(length(list.dirs(path = paste(documents_folder_path,
                                          "/SplitR", sep = ''),
                             full.names = TRUE, recursive = FALSE)) > 0, TRUE, FALSE)
+  
+  # Get path of SplitR folder
+  if (SplitR_dir_exists == TRUE) {
+    SplitR_path <- file.path(paste(documents_folder_path, "/SplitR", sep = ''))
+  }
+  
+  # Check for existence of .SplitR file
+  SplitR_file_exists <- file.exists(paste(SplitR_path, "/.SplitR", sep = ''))
+  
+  # Get path of .SplitR file
+  if (SplitR_file_exists == TRUE) {
+    SplitR_file_path <- file.path(paste(SplitR_path, "/.SplitR", sep = ''))
+  }
+  
+  # Access the .SplitR file
+  SplitR_file <- file(paste("file://",
+                            system("cd ~ ; pwd", intern = TRUE),
+                            gsub("~", "", SplitR_path),
+                            "/.SplitR", sep = ''))
+  
+  # Get .SplitR file as character vector
+  SplitR_file_text <- readLines(SplitR_file)
+  
+  # Check that the 'SplitR_file_text' object was created
+  if (exists("SplitR_file_text")) SplitR_file_text_created <- TRUE
+  
+  # Close and destroy the connection to file
+  close(SplitR_file)
+  
+  # Find vector positions of headings in .SplitR file
+  for (i in 1:length(SplitR_file_text)) {
+    if (i == 1) positions <- mat.or.vec(nr = 5, nc = 1)
+    if (grepl("paths", SplitR_file_text[i]) == TRUE) positions[1] <- i
+    if (grepl("platform", SplitR_file_text[i]) == TRUE) positions[2] <- i
+    if (grepl("executables", SplitR_file_text[i]) == TRUE) positions[3] <- i
+    if (grepl("met files", SplitR_file_text[i]) == TRUE) positions[4] <- i
+    if (grepl("log", SplitR_file_text[i]) == TRUE) positions[5] <- i
+  }
+  
+  # Add to heading
+  if (mode == "add") {
+    insert.at <- function(a, pos, ...){
+      dots <- list(...)
+      stopifnot(length(dots) == length(pos))
+      result <- vector("list", 2 * length(pos) + 1)
+      result[c(TRUE, FALSE)] <- split(a, cumsum(seq_along(a) %in% (pos + 1)))
+      result[c(FALSE, TRUE)] <- dots
+      unlist(result)
+    }
+    SplitR_file_text <- insert.at(SplitR_file_text, positions[heading_order] + 1, string)
+    
+    # Overwrite the original .SplitR file with new one
+    cat(SplitR_file_text, file = SplitR_file_path, sep = "\n")
+  }
+  
+  # Remove from heading
+
+  
 }
