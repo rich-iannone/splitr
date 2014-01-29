c32c154923295b904a2d9ba1bc2653b21cccddad
rich-iannone <riannone@me.com>
2014-01-29 00:21:55 -0800

    Add function call to ‘SplitR.get.exec’ to get exec files

--- R/SplitR.init.R
+++ R/SplitR.init.R
@@ -52,9 +52,9 @@ SplitR.init <- function(install_folders = TRUE,
   
     # Get date and time of initialization
     create_time <- Sys.time()
-    
-    # Get the platform name
-    
+
+    # Get executable files
+    SplitR.get.exec()
     
     # Initialize the .SplitR plaintext file
     cat("paths", "-----",
