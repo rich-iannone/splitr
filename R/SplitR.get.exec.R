2249e8cc47b346830dea793c2f640a861278a62e
rich-iannone <riannone@me.com>
2014-01-28 23:16:20 -0800

    Check for existence of Documents folder in user home

--- R/SplitR.get.exec.R
+++ R/SplitR.get.exec.R
@@ -1,3 +1,10 @@
 SplitR.get.exec <- function(){
+
+  # Check for existence of Documents folder in user home folder
+  if(length(grep("/Documents$",
+                 list.dirs(path = "~", full.names = TRUE,
+                           recursive = FALSE))) > 0) {
+    documents_folder_path <- paste("~", "/Documents", sep = '')
+  }
   
 }
