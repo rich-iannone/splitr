99ddfc7ee9ce5db54f6197b45be01773359684b5
rich-iannone <riannone@me.com>
2014-01-30 08:41:27 -0800

    Determine the current project number with getwd()

--- R/project.open.R
+++ R/project.open.R
@@ -3,6 +3,10 @@ project.open <- function(list_select = TRUE, project = NULL){
   # Get available project numbers
   project_numbers <- seq(from = 1, to = nrow(project.list()), by = 1)
   
+  # Determine the current project number
+  
+  # Get the current wd, strip the path to the project subfolder level
+  current_wd_project <- gsub("^(.*)(Projects*)", "\\2", getwd())
   # Allow for the selection of the project to open through the display of the project list
   if (list_select == TRUE & is.null(project)) {
     
