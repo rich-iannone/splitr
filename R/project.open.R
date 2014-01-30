ea1dbf03fdd925f3453904b50940b3fbe3814c10
rich-iannone <riannone@me.com>
2014-01-29 23:39:49 -0800

    Open if statement that enters an interactive mode

--- R/project.open.R
+++ R/project.open.R
@@ -2,9 +2,9 @@ project.open <- function(list_select = TRUE, project = NULL){
   
   # Get available project numbers
   project_numbers <- seq(from = 1, to = nrow(project.list()), by = 1)
-    # Ask to save the current workspace to an .Rdata file in its project directory
   
-  
+  # Allow for the selection of the project to open through the display of the project list
+  if (list_select == TRUE) {
     # Clear out objects in the global workspace
   
   
