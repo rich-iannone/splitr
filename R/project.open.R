0cbf686f6449ce658e4682508a4e2a8a761fd958
rich-iannone <riannone@me.com>
2014-01-29 23:40:40 -0800

    All user to enter a number for the project to open

--- R/project.open.R
+++ R/project.open.R
@@ -8,6 +8,12 @@ project.open <- function(list_select = TRUE, project = NULL){
     
     # Display the project list
     print(project.list())
+    
+    # Allow for user to enter a number corresponding to the project to open
+    project_number_to_open <-
+      readline(paste("Which project number would you like to open? [1-",
+                     paste(nrow(project.list()), sep = ''), "] ", sep = ''))
+    project_number_to_open <- as.numeric(project_number_to_open)
     # Clear out objects in the global workspace
   
   
