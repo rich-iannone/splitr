21848d493e72a4f34b8e4e17781e887f53e2d9a8
rich-iannone <riannone@me.com>
2014-02-04 08:00:13 -0800

    Add arguments to function

--- R/hysplit.dispersion.R
+++ R/hysplit.dispersion.R
@@ -1,4 +1,21 @@
-hysplit.dispersion <- function(use_default_config = TRUE){ 
+hysplit.dispersion <- function(use_default_config = TRUE,
+                               start_lat_deg,
+                               start_long_deg,
+                               start_height_m_ASL,
+                               simulation_duration_h = 24,
+                               backward_running = FALSE,
+                               met_type,
+                               vertical_motion_option = 0,
+                               top_of_model_domain_m = 20000,
+                               run_type,
+                               run_day = NULL,
+                               run_range = NULL,
+                               run_years = NULL,
+                               daily_hours_to_start,
+                               path_met_files,
+                               path_output_files,
+                               path_wd,
+                               path_executable){ 
   
   # Define package requirements
   require(lubridate)
