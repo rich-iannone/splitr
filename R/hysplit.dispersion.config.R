0c14be7701ed0fe8ea26a080b846b098c7276d65
rich-iannone <riannone@me.com>
2014-02-04 12:45:49 -0800

    Create default config file for disp runs in wd

--- R/hysplit.dispersion.config.R
+++ R/hysplit.dispersion.config.R
@@ -1,7 +1,20 @@
 hysplit.dispersion.config <- function(create_default_config = FALSE,
                                       interactive_mode = TRUE){
   
-  
+  # Create default configuration file for dispersion runs and place it in the
+  # working directory
+  if (create_default_config == TRUE){
+  cat(" &SETUP", " tratio = 0.75,", " initd = 0,", " kpuff = 0,", " khmax = 9999,",
+   " kmixd = 0,", " kmix0 = 250,", " kzmix = 0,", " kdef = 0,", " kbls = 1,",
+   " kblt = 2,", " conage = 48,", " numpar = 2500,", " qcycle = 0.0,", " efile = '',",
+   " tkerd = 0.18,", " tkern = 0.18,", " ninit = 1,", " ndump = 0,", " ncycl = 0,",
+   " pinpf = 'PARINIT',", " poutf = 'PARDUMP',", " mgmin = 10,", " kmsl = 0,",
+   " maxpar = 10000,", " cpack = 1,", " cmass = 0,", " dxf = 1.0,", " dyf = 1.0,",
+   " dzf = 0.01,", " ichem = 0,", " maxdim = 1,", " kspl = 1,", " krnd = 6,",
+   " frhs = 1.0,", " frvs = 0.01,", " frts = 0.10,", " frhmax = 3.0,", " splitf = 1.0,",
+   " /", sep = "\n", file = "SETUP.CFG")
+  }
+    
   
 }
                                       
