7ac3e7ad28b920d01abb8847c0f5d80532ab2734
rich-iannone <riannone@me.com>
2014-02-04 14:02:42 -0800

    Add file for ‘hysplit.dispersion.define.R’ function

---
+++ R/hysplit.dispersion.define.R
@@ -0,0 +1,18 @@
+hysplit.dispersion.define <- function(define,
+                                      ){
+  
+  
+  # Define species (names, properties)
+  
+  
+  
+  # Define emissions (fixed location, rate, hours, start time, must tie to an already
+  # defined species or define a new one here)
+  
+  
+  
+  # Define sampling grids (fixed location, size/spacing, rate of sampling, sampling times)
+  
+  
+  
+}
