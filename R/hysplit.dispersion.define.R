fcbb0d3fc7fbebbb4432b2b10841dd51d2568c21
rich-iannone <riannone@me.com>
2014-02-04 23:27:18 -0800

    Ask for the name of the species

--- R/hysplit.dispersion.define.R
+++ R/hysplit.dispersion.define.R
@@ -3,6 +3,10 @@ hysplit.dispersion.define <- function(define){
   
   # Define species (names, properties)
   if (define == 'species'){
+    
+    # Ask for the name of the species
+    species_name <-
+      readline("What is the name of the species? ")
   
   # Define emissions (fixed location, rate, hours, start time, must tie to an already
   # defined species or define a new one here)
