2ef849d3b8ace6109fd7d4ebd6894eb35d63d285
rich-iannone <riannone@me.com>
2014-02-05 16:29:03 -0800

    Open if block for emissions source

--- R/hysplit.dispersion.define.R
+++ R/hysplit.dispersion.define.R
@@ -367,8 +367,10 @@ hysplit.dispersion.define <- function(define){
   
   # Define emissions (fixed location, rate, hours, start time, must tie to an already
   # defined species or define a new one here)
+  if (define == 'emissions'){
   
   
+  }
   
   # Define sampling grids (fixed location, size/spacing, rate of sampling, sampling times)
   
