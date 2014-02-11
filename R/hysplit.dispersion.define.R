462ebe3b0e8fc03845de7639fcc5bdb4019c19e8
rich-iannone <riannone@me.com>
2014-02-11 07:28:27 -0800

    Validate input of the date/time string

--- R/hysplit.dispersion.define.R
+++ R/hysplit.dispersion.define.R
@@ -684,6 +684,15 @@ hysplit.dispersion.define <- function(define){
                          "Use the format YYYY-MM-DD HH:MM", "\n",
                          sep = '')))
     
+    # Validate the input of the date and time string
+    if (grepl("[1-2][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9] [0-2][0-9]:[0-9][0-9]",
+              grid_end_time, perl = TRUE) == TRUE) {
+      grid_end_time_valid_1 <- TRUE
+    } else if (grepl("[1-2][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9] [0-2][0-9]:[0-9][0-9]",
+                     grid_end_time, perl = TRUE) == FALSE) {
+      grid_end_time_valid_1 <- FALSE
+    }
+    
     
     # Close grids block
   }
