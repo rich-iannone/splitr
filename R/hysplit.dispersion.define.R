c3deab97aab5cf54587ebf9a84ff5b9b6b695ef6
rich-iannone <riannone@me.com>
2014-02-11 21:46:48 -0800

    Provide summary of chosen options; ask if okay

--- R/hysplit.dispersion.define.R
+++ R/hysplit.dispersion.define.R
@@ -478,6 +478,17 @@ hysplit.dispersion.define <- function(define){
       readline(paste(cat("Provide the rate of emissions in mass units per hour.", "\n",
                          sep = '')))
     
+    
+    # Provide a summary of the chosen options and ask if the chosen options are acceptable
+    acceptable_emissions <-
+      readline(paste(cat("The plan. Adding emissions source: ", emissions_name, "\n",
+                         "----------------------------------", "\n", 
+                         "Start Date/Time: ", emissions_start_time, "\n",
+                         "       Duration: ", emissions_duration, " h", "\n",
+                         " Emissions Rate: ", emissions_rate, " 1/h", "\n",
+                         "----------------------------------", "\n",
+                         "This is what will be set. Okay? [y/n]: ",
+                         sep = '')))
     # Close emissions block
   }
   
