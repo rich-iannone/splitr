fc21ae3b8046741746f103951ffafc3772e002bc
rich-iannone <riannone@me.com>
2014-02-13 12:23:01 -0800

    Specify a full path with file name for writing grid info

--- R/hysplit.dispersion.define.R
+++ R/hysplit.dispersion.define.R
@@ -826,7 +826,7 @@ hysplit.dispersion.define <- function(define){
           grid_start_time_char, "\n",
           grid_end_time_char, "\n",
           sampling_interval_type_no, " ", sampling_interval_rate_char, "\n",
-          file = "~/Documents/SplitR", append = TRUE,
+          file = "~/Documents/SplitR/grids", append = TRUE,
           sep = '')
     }
 
