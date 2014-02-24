8bdd57a85b32f013db1a9071306ccc61d0083f4b
rich-iannone <riannone@me.com>
2014-02-24 07:53:10 -0800

    Add defaults for paths in argument list

--- R/hysplit.dispersion.R
+++ R/hysplit.dispersion.R
@@ -10,8 +10,10 @@ hysplit.dispersion <- function(start_lat_deg = 49.289328, start_long_deg = -123.
                                emissions = c(1),
                                species = c(1),
                                grids = c(1),
-                               path_met_files, path_output_files,
-                               path_wd, path_executable){ 
+                               path_met_files = "~/Documents/SplitR/Met/",
+                               path_output_files = "~/Documents/SplitR/Output/",
+                               path_wd = "~/Documents/SplitR/Working/",
+                               path_executable = "~/Documents/SplitR/Exec/hycs_std"){ 
   
   # Define package requirements
   require(lubridate)
