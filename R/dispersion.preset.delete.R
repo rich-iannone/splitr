a69f8316c02b53e9c89ef7b98f8116023c15a5fc
rich-iannone <riannone@me.com>
2014-02-28 09:02:59 -0800

    Extract list of entry blocks from file

--- R/dispersion.preset.delete.R
+++ R/dispersion.preset.delete.R
@@ -17,9 +17,32 @@ dispersion.preset.delete <- function(read, numbers = NULL, interactive = TRUE){
     
     if (read == "emissions"){
       
+      from_file <- as.vector(read.table("~/Documents/SplitR/emissions", sep = "\n"))
+      
+      number_of_entries <- nrow(from_file) / 6
+      
+      seq_of_entries <- 1:number_of_entries
+      
+      list.from_file <- vector(mode = "list", length = number_of_entries)
     if (read == "grids"){
       
+      from_file <- as.vector(read.table("~/Documents/SplitR/grids", sep = "\n"))
+      
+      number_of_entries <- nrow(from_file) / 12
+      
+      seq_of_entries <- 1:number_of_entries
+      
+      list.from_file <- vector(mode = "list", length = number_of_entries)
+      
     if (read == "species"){
+      
+      from_file <- as.vector(read.table("~/Documents/SplitR/species", sep = "\n"))
+      
+      number_of_entries <- nrow(from_file) / 7
+      
+      seq_of_entries <- 1:number_of_entries
+      
+      list.from_file <- vector(mode = "list", length = number_of_entries)
       
   # End of function
 }
