0ff17ba7800ff6ebc80f25002ef3f0179a3430ee
rich-iannone <riannone@me.com>
2014-02-28 08:59:59 -0800

    Validate supplied digits for duplicate values

--- R/dispersion.preset.delete.R
+++ R/dispersion.preset.delete.R
@@ -1,6 +1,9 @@
 dispersion.preset.delete <- function(read, numbers = NULL, interactive = TRUE){
   
-
+  # Validate supplied digits for duplicates
+  if (!is.null(numbers)){
+    no_duplicates <- ifelse(anyDuplicated(numbers) == 0,
+                            TRUE, FALSE)
   
   # End of function
 }
