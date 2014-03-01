7c3864cf3d8ded0e1f906951588ab9f930f540d6
rich-iannone <riannone@me.com>
2014-02-28 23:33:27 -0800

    Remove unused arguments from function

--- R/SplitR.init.R
+++ R/SplitR.init.R
@@ -1,8 +1,5 @@
-SplitR.init <- function(install_folders = TRUE,
-                        install_in_documents = TRUE,
-                        other_install_path){
+SplitR.init <- function(install_folders = TRUE){
 
-  
   #---- Check for existence and path of SplitR folder
   #
   # 
