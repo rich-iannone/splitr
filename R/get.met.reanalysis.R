4e35c9eb7d6b391c1346f215448cc276f2f2adc4
rich-iannone <riannone@me.com>
2014-03-02 14:42:56 -0800

    Add method to download list of reanalysis files by name

--- R/get.met.reanalysis.R
+++ R/get.met.reanalysis.R
@@ -2,6 +2,21 @@ get.met.reanalysis <- function(files = NULL,
                                years = NULL,
                                months = NULL,
                                path_met_files = "~/Documents/SplitR/Met/"){ 
-
-
+  
+  
+  # Download list of files by name
+  if (!is.null(files)){
+    
+    for (i in 1:length(files)){
+      download.file(url = paste("ftp://arlftp.arlhq.noaa.gov/archives/reanalysis/",
+                                files[i], sep = ''),
+                    destfile = paste(path_met_files, files[i], sep = ''),
+                    method = "auto",
+                    quiet = FALSE,
+                    mode = "w",
+                    cacheOK = TRUE)
+    }
+    
+  }
+  
 }
