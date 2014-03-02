7ccd8957ecfd3b458f199767c0f56dafc4003f5e
rich-iannone <riannone@me.com>
2014-03-01 17:08:09 -0800

    Add arguments and defaults to 'get.met.nam12'

--- R/get.met.nam12.R
+++ R/get.met.nam12.R
@@ -1,6 +1,6 @@
-get.met.nam12 <- function( ){ 
-  
-  
-  
+get.met.nam12 <- function(files = NULL,
+                          years = NULL,
+                          months = NULL,
+                          path_met_files = "~/Documents/SplitR/Met/"){
   
 }
