50e5c420b476fd8f522823cacd85b84a8f5c9ad8
rich-iannone <riannone@me.com>
2014-03-01 16:56:05 -0800

    Add arguments and defaults to 'get.met.reanalysis'

--- R/get.met.reanalysis.R
+++ R/get.met.reanalysis.R
@@ -1,6 +1,7 @@
-get.met.reanalysis <- function( ){ 
-  
-  
-  
-  
+get.met.reanalysis <- function(files = NULL,
+                               years = NULL,
+                               months = NULL,
+                               path_met_files = "~/Documents/SplitR/Met/"){ 
+
+
 }
