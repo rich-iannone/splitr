fc700c944f0045b6bf19ec51b878c65bd0f7128f
rich-iannone <riannone@me.com>
2014-03-02 21:51:36 -0800

    Initiate download of provided list of met files

--- R/get.met.gdas1.R
+++ R/get.met.gdas1.R
@@ -3,7 +3,20 @@ get.met.gdas1 <- function(files = NULL,
                           months = NULL,
                           path_met_files = "~/Documents/SplitR/Met/"){
   
-  
+  # Download list of GDAS1 met files by name
+  if (!is.null(files)){
+    
+    for (i in 1:length(files)){
+      download.file(url = paste("ftp://arlftp.arlhq.noaa.gov/archives/gdas1/",
+                                files[i], sep = ''),
+                    destfile = paste(path_met_files, files[i], sep = ''),
+                    method = "auto",
+                    quiet = FALSE,
+                    mode = "w",
+                    cacheOK = TRUE)
+    }
+    
+  }
   
   
 }
