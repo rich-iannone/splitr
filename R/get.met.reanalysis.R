9c5f3d1bcef3658405a164e147384d965e9404f8
rich-iannone <riannone@me.com>
2014-03-02 20:56:23 -0800

    Remove 'months' argument from function

--- R/get.met.reanalysis.R
+++ R/get.met.reanalysis.R
@@ -1,6 +1,5 @@
 get.met.reanalysis <- function(files = NULL,
                                years = NULL,
-                               months = NULL,
                                path_met_files = "~/Documents/SplitR/Met/"){ 
   
   
