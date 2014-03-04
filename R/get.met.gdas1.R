de68f67d77f3c28c86db75721c4b6f8dd4aa1ddb
rich-iannone <riannone@me.com>
2014-03-03 16:22:21 -0800

    Remove obsolete code

--- R/get.met.gdas1.R
+++ R/get.met.gdas1.R
@@ -54,49 +54,4 @@ get.met.gdas1 <- function(files = NULL,
   
 
   
-  # Download one or more years of GDAS1 met files
-#   if (!is.null(years)){
-#       
-#     months_3_letter <- c("jan", "feb", "mar", "apr", "may", "jun",
-#                            "jul", "aug", "sep", "oct", "nov", "dec")
-#     for (i in 1:length(years)){
-#       
-#       # Construct list of files for the year
-#       files_for_year <- vector(mode = "character",
-#                                length = ifelse(years[i] %% 4 == 0, 60, 59))
-# 
-#       
-#       for (j in 1:length(files_for_year)){
-#         for (k in 1:length(months_3_letter)){
-#           if (years[i] %% 4 != 0 & months_3_letter[k] == "feb"){
-#             weeks_in_month <- seq(from = 1, to = 4, by = 1)
-#           } else {
-#             weeks_in_month <- seq(from = 1, to = 5, by = 1)
-#           }
-#           for (l in weeks_in_month){
-#             files_for_year[j] <-
-#             paste("gdas1.", months_3_letter[k], gsub("^..", "", years[i]), ".w",
-#                   weeks_in_month[l], sep = '')
-#           }
-#           
-#         }
-#       }
-#     }
-#       
-#         
-#       for (k in 1:length(files_for_year)){
-#         download.file(url = paste("ftp://arlftp.arlhq.noaa.gov/archives/gdas1/",
-#                                   files_for_year[k], sep = ''),
-#                       destfile = paste(path_met_files, files_for_year[k], sep = ''),
-#                       method = "auto",
-#                       quiet = FALSE,
-#                       mode = "w",
-#                       cacheOK = TRUE)
-#       }
-#       
-#     }
-#     
-#   }
-
-
 }
