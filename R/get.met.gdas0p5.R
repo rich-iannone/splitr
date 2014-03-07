709b4d90701b3127f1429ca5149097251057453d
rich-iannone <riannone@me.com>
2014-03-06 23:38:06 -0800

    Download the 'listing' file from the NOAA FTP server

--- R/get.met.gdas0p5.R
+++ R/get.met.gdas0p5.R
@@ -4,6 +4,15 @@ get.met.gdas0p5 <- function(files = NULL,
                             days = NULL
                             path_met_files = "~/Documents/SplitR/Met/"){
   
+  # Download the 'listing' file from NOAA server
+  # It contains a list of GDAS0p5 files currently available on the server
+  download.file(url = paste("ftp://arlftp.arlhq.noaa.gov/archives/gdas0p5/",
+                            "listing", sep = ''),
+                destfile = paste(path_met_files, "listing", sep = ''),
+                method = "auto",
+                quiet = TRUE,
+                mode = "w",
+                cacheOK = TRUE)
   
   
   
