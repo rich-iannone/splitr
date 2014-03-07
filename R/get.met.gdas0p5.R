a2d9873110fbac5d399538453ad038e8a1bb35ed
rich-iannone <riannone@me.com>
2014-03-07 14:15:25 -0800

    Make correction to syntax of function arguments

--- R/get.met.gdas0p5.R
+++ R/get.met.gdas0p5.R
@@ -1,7 +1,7 @@
 get.met.gdas0p5 <- function(files = NULL,
                             years = NULL,
                             months = NULL,
-                            days = NULL
+                            days = NULL,
                             path_met_files = "~/Documents/SplitR/Met/"){
   
   require(lubridate)
