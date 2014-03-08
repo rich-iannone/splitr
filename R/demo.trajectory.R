75514ef5a8405caf686c09a898ad7fca3321d222
rich-iannone <riannone@me.com>
2014-03-07 19:16:54 -0800

    Add comments; define a project called "Demo Project"

--- R/demo.trajectory.R
+++ R/demo.trajectory.R
@@ -3,5 +3,16 @@ demo.trajectory <- function(){
   # This function is meant to show the user how a trajectory project is carried out
   # There are step-by-step prompts, showing the user which directories were made,
   # what functions were run, and where the output went
+
+
+  # Welcome to the SplitR trajectory demo! The aim of this walkthrough
+  # is give you all you need to run HYSPLIT trajectory runs.
+  
+  # The first thing to do is to define a project.
+  # Let's create a project and call it "Demo Project".
+  
+  project.define("Demo Project")
+  
+    
   
 }
