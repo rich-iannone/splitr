0ff9e11fbb29ce5e4df8df7b928513b663d32a6c
rich-iannone <riannone@me.com>
2014-03-07 17:08:03 -0800

    Add comment explaining the purpose of this function

--- R/demo.dispersion.R
+++ R/demo.dispersion.R
@@ -1,3 +1,7 @@
 demo.dispersion <- function(){
+ 
+  # This function is meant to show the user how a dispersion project is carried out
+  # There are step-by-step prompts, showing the user which directories were made,
+  # what functions were run, and where the output went
   
 }
