093fa28daadc74627b80ac54ba6d5f30c6ed1909
rich-iannone <riannone@me.com>
2014-03-08 07:32:33 -0800

    Add statements and function to set up a project

--- R/demo.trajectory.R
+++ R/demo.trajectory.R
@@ -3,16 +3,37 @@ demo.trajectory <- function(){
   # This function is meant to show the user how a trajectory project is carried out
   # There are step-by-step prompts, showing the user which directories were made,
   # what functions were run, and where the output went
-
-
+  
   # Welcome to the SplitR trajectory demo! The aim of this walkthrough
   # is give you all you need to run HYSPLIT trajectory runs.
   
-  # The first thing to do is to define a project.
-  # Let's create a project and call it "Demo Project".
+  # Let's have a look around at the the folders that were generated from using the
+  # 'SplitR.init' function. Five folders were made: Exec, Met, Output, Projects, and Working.
   
+  setwd("~/Documents/SplitR")
+  
+  dirs <- as.data.frame(list.dirs(path = "~/Documents/SplitR", recursive = FALSE))
+  colnames(dirs) <- "Folders"
+  dirs
+  
+  # The first step toward running models is to define a project.
+  # Let's create a project and call it "Demo Project".
+    
   project.define("Demo Project")
   
-    
+  # You can see a list of all projects with 'project.list'
+  
+  project.list()
+  
+  # Once the project is defined, open that project with 'project.open'
+  
+  project.open(project_name = "Demo Project")
+  
+  # Notice that when using 'project.open', the working directory moves
+  # to that of the project.
+  
+  getwd()
+  
+  
   
 }
