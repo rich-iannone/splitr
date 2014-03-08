ab099f58c6c77ff98a2294a1e7107566fe155e24
rich-iannone <riannone@me.com>
2014-03-07 17:08:13 -0800

    Add comment explaining the purpose of this function

--- R/demo.trajectory.R
+++ R/demo.trajectory.R
@@ -1,3 +1,7 @@
 demo.trajectory <- function(){
   
+  # This function is meant to show the user how a trajectory project is carried out
+  # There are step-by-step prompts, showing the user which directories were made,
+  # what functions were run, and where the output went
+  
 }
