6759c04f9dfe8c763ed4695b1e6fa7dd812db41c
rich-iannone <riannone@me.com>
2014-03-09 13:10:08 -0700

    Check for existence of 'Documents' folder in user home

--- R/project.archive.R
+++ R/project.archive.R
@@ -1,1 +1,8 @@
 project.archive <- function( ){ 
+  
+  # Check for existence of Documents folder in user home folder
+  if(length(grep("/Documents$",
+                 list.dirs(path = "~", full.names = TRUE,
+                           recursive = FALSE))) > 0) {
+    documents_folder_path <- paste("~", "/Documents", sep = '')
+  }
