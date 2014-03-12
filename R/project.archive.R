05b8c7c87caaa1f38bdd1f12d3f7e822ce4b0df3
rich-iannone <riannone@me.com>
2014-03-12 09:08:50 -0700

    Make correction to path in system statement

--- R/project.archive.R
+++ R/project.archive.R
@@ -79,7 +79,7 @@ project.archive <- function(project_name = NULL){
         # Move contents of archived project to the 'Archive' folder
         system(paste("cd ~; cd ~/Documents/SplitR/Archive ; mkdir '", folder_name_project_to_archive,
                      "' ; cd ../Projects ; cp '", folder_name_project_to_archive, "'/* '../Archive/",
-                     folder_name_project_to_archive, "' ; cd ~/Documents/SplitR/Archive ; ",
+                     folder_name_project_to_archive, "' ; cd ~/Documents/SplitR/Projects ; ",
                      "cd '", folder_name_project_to_archive, "' ; rm *",
                      sep = ''))
         
