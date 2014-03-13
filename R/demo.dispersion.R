917f37dc36613a9ec4071d8ba8fa1230f69110a9
rich-iannone <riannone@me.com>
2014-03-13 08:40:24 -0700

    Add preliminary blocks showing user the project.* functions

--- R/demo.dispersion.R
+++ R/demo.dispersion.R
@@ -1,5 +1,98 @@
 demo.dispersion <- function(){
  
+  # First, delete any previous instances of the 'Demo Disp Project' that were created
+  # when using this function previously
+  
+  project.archive("Demo Disp Project")
+  
+  setwd("~/Documents/SplitR")
+  
+  step_1 <-
+    readline(paste(cat("This demo will show you how a dispersion project is carried out.", "\n",
+                       "There are step-by-step prompts, showing which directories were made,", "\n",
+                       "what functions were run, and where the output was placed. It'll give", "\n",
+                       "you all that you need to set up some HYSPLIT dispersion models.", "\n",
+                       "(N.B. increase the height of the console as much you can.)", "\n",
+                       "\n",
+                       "Press <ENTER> to continue.",
+                       sep = '')))
+  
+  if (step_1 == "") cat()
+  
+  paste(cat("\n",
+            "-------------------------------------------------------------------------------", "\n",
+            "\n",
+            "Let's have a look at the the folders made from using the 'SplitR.init'", "\n",
+            "function. Five folders were made: Exec, Met, Output, Projects, and Working.", "\n",
+            "(Archive might also be there; it's generated with use of 'project.archive'.)", "\n",
+            "\n",
+            sep = ''))
+  
+  dirs <- as.data.frame(list.dirs(path = "~/Documents/SplitR", recursive = FALSE))
+  colnames(dirs) <- "Folders"
+  print(dirs)
+  
+  step_2 <-
+    readline(paste(cat("\n",
+                       "--------------------------------------------------------------",
+                       "-----------------", "\n",
+                       "The first step toward running models is to define a project.", "\n",
+                       "Let's create a project and call it \"Demo Disp Project\".", "\n",
+                       "\n",
+                       "Press <ENTER> to run the following:", "\n",
+                       "\n",
+                       "--> project.define(\"Demo Disp Project\")",
+                       sep = '')))
+  
+  if (step_2 == ""){
+    project.define("Demo Disp Project")
+  }
+  
+  step_3 <-
+    readline(paste(cat("\n",
+                       "--------------------------------------------------------------",
+                       "-----------------", "\n",
+                       "You can see a list of all projects with 'project.list'", "\n",
+                       "\n",
+                       "Press <ENTER> to run the following:", "\n",
+                       "\n",
+                       "--> project.list()",
+                       sep = '')))
+  
+  if (step_3 == ""){
+    print(project.list())
+  }
+  
+  step_4 <-
+    readline(paste(cat("\n",
+                       "--------------------------------------------------------------",
+                       "-----------------", "\n",
+                       "Once the project is defined, open that project with 'project.open'", "\n",
+                       "\n",
+                       "Press <ENTER> to run the following:", "\n",
+                       "\n",
+                       "--> project.open(project_name = \"Demo Disp Project\")",
+                       sep = '')))
+  
+  if (step_4 == ""){
+    project.open(project_name = "Demo Disp Project")
+  }
+  
+  step_5 <-
+    readline(paste(cat("\n",
+                       "--------------------------------------------------------------",
+                       "-----------------", "\n",
+                       "You might notice that when using 'project.open', the working ",
+                       "directory moves", "\n",
+                       "to that of the project.", "\n",
+                       "\n",
+                       "Press <ENTER> to get the working directory", "\n",
+                       sep = '')))
+  
+  if (step_5 == ""){
+    print(getwd())
+  }
+  
   
   
   
