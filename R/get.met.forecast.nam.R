927bf6cdb5d39c953ba8e444f0f19811df244ee0
rich-iannone <riannone@me.com>
2014-04-23 00:55:19 -0700

    Download today's namf file from FTP server

--- R/get.met.forecast.nam.R
+++ R/get.met.forecast.nam.R
@@ -11,4 +11,17 @@ get.met.forecast.nam <- function(path_met_files = "~/Documents/SplitR/Met/"){
   # Get today's date and write in format equivalent to FTP directories 
   today <- gsub("-", "", Sys.Date())
   
+  # Download today's namf file
+  # -- CONUS, 12 km, 3 hrly, pressure levels, 48 h forecast
+  
+  if (today %in% forecast_dirs){
+    download.file(url = paste("ftp://arlftp.arlhq.noaa.gov/forecast/",
+                              today, "/hysplit.t00z.namf", sep = ''),
+                  destfile = paste(path_met_files, files[i], sep = ''),
+                  method = "auto",
+                  quiet = FALSE,
+                  mode = "w",
+                  cacheOK = TRUE)
+  }
+  
 }
