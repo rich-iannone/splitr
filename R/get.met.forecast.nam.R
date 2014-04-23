c89405b7cde9e73b37cfca3ca49215bf358570bc
rich-iannone <riannone@me.com>
2014-04-23 00:38:23 -0700

    Determine which forecast dirs are currently on FTP server

--- R/get.met.forecast.nam.R
+++ R/get.met.forecast.nam.R
@@ -1,4 +1,12 @@
 get.met.forecast.nam <- function(path_met_files = "~/Documents/SplitR/Met/"){
   
+  require("RCurl") 
+  
+  # Establish which forecast dirs are currently available on the server
+  forecast_dir <-
+    grep("^[0-9].*$",
+         unlist(strsplit(getURL("ftp://arlftp.arlhq.noaa.gov/forecast/",
+                                dirlistonly = TRUE), "\n")), value = TRUE)
+    
   
 }
