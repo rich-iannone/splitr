f8b1c1fde6dbabfb4cf46601149b0e304e1a95a0
rich-iannone <riannone@me.com>
2014-04-23 00:54:37 -0700

    Modify name of object

--- R/get.met.forecast.nam.R
+++ R/get.met.forecast.nam.R
@@ -3,7 +3,7 @@ get.met.forecast.nam <- function(path_met_files = "~/Documents/SplitR/Met/"){
   require("RCurl") 
   
   # Establish which forecast dirs are currently available on the server
-  forecast_dir <-
+  forecast_dirs <-
     grep("^[0-9].*$",
          unlist(strsplit(getURL("ftp://arlftp.arlhq.noaa.gov/forecast/",
                                 dirlistonly = TRUE), "\n")), value = TRUE)
