62c9944d2a3fac71d40a6e560251c8ac31c5186e
rich-iannone <riannone@me.com>
2014-04-23 00:40:47 -0700

    Correct 'download.file' statement (use correct dir name)

--- R/get.met.edas40.R
+++ R/get.met.edas40.R
@@ -3,7 +3,17 @@ get.met.edas40 <- function(files = NULL,
                            months = NULL,
                            path_met_files = "~/Documents/SplitR/Met/"){
   
+  require(lubridate)
   
+  # Download the 'listing' file from NOAA server
+  # It contains a list of EDAS40 files currently available on the server
+  download.file(url = paste("ftp://arlftp.arlhq.noaa.gov/archives/edas40/",
+                            "listing", sep = ''),
+                destfile = paste(path_met_files, "listing", sep = ''),
+                method = "auto",
+                quiet = TRUE,
+                mode = "w",
+                cacheOK = TRUE)
   
   
 }
