36e9b8f365014cc88759339d5befcaf75a58f411
rich-iannone <riannone@me.com>
2014-06-02 00:11:41 -0700

    Add require statements for 'RCurl' and 'lubridate' packages

--- R/get.met.nam12.R
+++ R/get.met.nam12.R
@@ -2,5 +2,9 @@ get.met.nam12 <- function(files = NULL,
                           years = NULL,
                           months = NULL,
                           path_met_files = "~/Documents/SplitR/Met/"){
+ 
+  # Add require statements
+  require(RCurl) 
+  require(lubridate)
   
 }
