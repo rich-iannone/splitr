c8f3b1469ea821a66912c051ada42a0913a695dd
rich-iannone <riannone@me.com>
2014-06-07 00:01:49 -0700

    Remove unneeded statements

--- R/dispersion.stats.R
+++ R/dispersion.stats.R
@@ -31,12 +31,6 @@ dispersion.stats <- function(dispersion_df, stats){
     # Return the stats table containing the five number summary at every hour
     return(stats_table)
     
-    stats_small_table$min <- fivenum(dispersion.height$height, na.rm = TRUE)[1]
-    stats_small_table$lower_hinge <- fivenum(dispersion.height$height, na.rm = TRUE)[2]
-    stats_small_table$median <- fivenum(dispersion.height$height, na.rm = TRUE)[3]
-    stats_small_table$upper_hinge <- fivenum(dispersion.height$height, na.rm = TRUE)[4]
-    stats_small_table$max <- fivenum(dispersion.height$height, na.rm = TRUE)[5]
-    stats_small_table$hour <- i
     
     stats_table <- rbind(stats_table, stats_small_table)
     
