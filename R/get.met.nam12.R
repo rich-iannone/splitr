dbb18bb44e1d24c1deae5470aa0aead4495f1abc
rich-iannone <riannone@me.com>
2014-06-15 14:31:22 -0700

    Add roxygen2 statements at head of script

--- R/get.met.nam12.R
+++ R/get.met.nam12.R
@@ -1,3 +1,11 @@
+#' Get NAM12 meteorology data files
+#'
+#' @param files 
+#' @param years 
+#' @param months 
+#' @param path_met_files 
+#' @export get.met.nam12
+
 get.met.nam12 <- function(files = NULL,
                           years = NULL,
                           months = NULL,
