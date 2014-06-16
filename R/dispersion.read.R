24ef3a65bea8cff01f8a609c863e30ac7a514c07
rich-iannone <riannone@me.com>
2014-06-15 23:47:52 -0700

    Add description field and info for roxygen2

--- R/dispersion.read.R
+++ R/dispersion.read.R
@@ -1,5 +1,5 @@
 #' Read HYSPLIT dispersion output files
-#'
+#' @description The function takes HYSPLIT dispersion output files in a specified output directory and processes all files into a data frame object.
 #' @param path_output_files 
 #' @export dispersion.read
 #' @examples
