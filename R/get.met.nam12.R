b55d0049ec1743531efd90317d9ab9fadad55a59
rich-iannone <riannone@me.com>
2014-06-15 23:48:24 -0700

    Add description field and info for roxygen2

--- R/get.met.nam12.R
+++ R/get.met.nam12.R
@@ -1,5 +1,5 @@
 #' Get NAM12 meteorology data files
-#'
+#' @description This function downloads NAM12 meteorology data files from the NOAA FTP server and saves them to a specified folder. Files can be downloaded either by specifying a list of filenames (in the form of "RP[YYYY][MM].gbl") or through bulk download of a year of files.
 #' @param files 
 #' @param years 
 #' @param months 
