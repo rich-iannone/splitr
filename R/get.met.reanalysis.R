15dca1cbfd5c96df757330201f5c3a8157ff314b
rich-iannone <riannone@me.com>
2014-06-15 23:48:29 -0700

    Add description field and info for roxygen2

--- R/get.met.reanalysis.R
+++ R/get.met.reanalysis.R
@@ -1,5 +1,5 @@
 #' Get reanalysis meteorology data files
-#'
+#' @description This function downloads reanalysis meteorology data files from the NOAA FTP server and saves them to a specified folder. Files can be downloaded either by specifying a list of filenames (in the form of "RP[YYYY][MM].gbl") or through bulk download of a year of files.
 #' @param files 
 #' @param years 
 #' @param path_met_files 
