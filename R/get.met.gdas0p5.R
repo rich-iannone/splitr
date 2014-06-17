6569f3efef959a41b6780408e55a0aec64606bc1
rich-iannone <riannone@me.com>
2014-06-17 07:28:12 -0700

    Remove default value for 'path_met_files' argument

--- R/get.met.gdas0p5.R
+++ R/get.met.gdas0p5.R
@@ -11,7 +11,7 @@ get.met.gdas0p5 <- function(files = NULL,
                             years = NULL,
                             months = NULL,
                             days = NULL,
-                            path_met_files = "~/Documents/SplitR/Met/"){
+                            path_met_files){
   
   # Add require statements
   require(RCurl) 
