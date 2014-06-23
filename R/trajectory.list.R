8a9fd5af082e1923298c7e33a2415a1ea6d2af2a
rich-iannone <riannone@me.com>
2014-06-23 07:32:45 -0700

    Add script for 'trajectory.list' function

---
+++ R/trajectory.list.R
@@ -0,0 +1,11 @@
+#' List HYSPLIT trajectory output archive files or folders
+#' @description The function lists the HYSPLIT trajectory output files that reside in a specified directory.
+#' @param output_folder the absolute path for the trajectory archive files (UNIX) or folders (Windows) is to be provided.
+#' @return data frame with information on trajectory model output data archives
+#' @export trajectory.list
+
+
+trajectory.list <- function(output_folder){
+  
+}
+
