07f6135f5979b9226b4d7bde2a9fdcac0f4b9c23
rich-iannone <riannone@me.com>
2014-06-24 00:46:32 -0700

    Get lat/lon and display in the data frame

--- R/trajectory.list.R
+++ R/trajectory.list.R
@@ -89,5 +89,21 @@ trajectory.list <- function(output_folder){
                                                          " | wc -l", sep = ''),
                                                    intern = TRUE))
     
+    lat <- gsub("^.*lat_([0-9\\.-]*)_.*", "\\1",
+                system(paste("unzip -l ",
+                             list.files(path = output_folder,
+                                        pattern = ".zip",
+                                        full.names = TRUE)[i],
+                             " | ls | less", sep = ''), intern = TRUE)[1])
+    
+    lon <- gsub("^.*long_([0-9\\.-]*)-height.*", "\\1",
+                system(paste("unzip -l ",
+                             list.files(path = output_folder,
+                                        pattern = ".zip",
+                                        full.names = TRUE)[i],
+                             " | ls | less", sep = ''), intern = TRUE)[1])
+    
+    trajectory_output_df[i,5] <- paste(lat, ", ", lon, sep = '')
+    
 }
 
