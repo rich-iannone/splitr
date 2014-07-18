985356acb94445a69da41dcc97b4534ba53b61f0
rich-iannone <riannone@me.com>
2014-07-17 19:40:46 -0700

    Add roxygen2 @param statement for the 'number_of_particles' argument

--- R/hysplit.dispersion.R
+++ R/hysplit.dispersion.R
@@ -12,6 +12,7 @@
 #' @param met_type an option to select meteorological data files. The options are "gdas1" (Global Data Assimilation System 1-degree resolution data) and 'reanalysis' (NCAR/NCEP global reanalysis data).
 #' @param vertical_motion_option a numbered option to select the method used to simulation vertical motion. The methods are: (0) input model data, (1) isobaric, (2) isentropic, (3) constant density, (4) isosigma, (5) from divergence, (6) remap MSL to AGL, (7) average data, and (8) damped magnitude.
 #' @param top_of_model_domain_m the upper limit of the model domain in meters.
+#' @param number_of_particles the number of particles released by a source during a model run
 #' @param run_type used to select whether models should be run for a single day ("day"), for one or more years ("years"), or within a specified date range ("range").
 #' @param run_day used when 'run_type' of 'day' is selected. The date format should be provided here as "YYYY-MM-DD".
 #' @param run_range used when 'run_type' of 'range' is selected. The date format should be provided here as "c("YYYY-MM-DD", "YYYY-MM-DD")".
