e976c59983d2e8a3bb7d91e711b87188d9f7db1c
rich-iannone <riannone@me.com>
2014-07-23 07:32:21 -0700

    Modify arguments used in 'hysplit.dispersion.animation'

--- R/hysplit.dispersion.animation.R
+++ R/hysplit.dispersion.animation.R
@@ -1,4 +1,8 @@
 hysplit.dispersion.animation <- function(dispersion_plots = NULL){
+hysplit.dispersion.animation <- function(path_plot_files,
+                                         frame_rate = 5,
+                                         movie_output_name = NULL,
+                                         path_output_files = NULL){
   
   
   
