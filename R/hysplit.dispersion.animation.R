04cda89043d0310b002102c875c0ad71604120b2
rich-iannone <riannone@me.com>
2014-07-27 01:14:24 -0700

    Use 'total_hours' as upper range boundary

--- R/hysplit.dispersion.animation.R
+++ R/hysplit.dispersion.animation.R
@@ -59,7 +59,7 @@ hysplit.dispersion.animation <- function(dispersion_df = NULL,
   
   # For each hour of particle positions in the 'dispersion_df' data frame, classify
   # the particles by the hour of release
-  for (i in 1:max(dispersion_df$hour)){
+  for (i in 1:total_hours){
     
     
     
