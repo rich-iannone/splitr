cf451067fcc455ecb0f3c283fa58eeebd6819e6c
rich-iannone <riannone@me.com>
2014-07-29 07:37:14 -0700

    Remove obsolete code

--- R/hysplit.dispersion.animation.R
+++ R/hysplit.dispersion.animation.R
@@ -140,23 +140,5 @@ hysplit.dispersion.animation <- function(dispersion_df = NULL,
     particle_df <- rbind(particle_df, df_from_vectors)
     
   }
-
-# Convert PDF files to 400 ppi JPEG files using ImageMagick
-
-
-# Crop the resulting JPEG files to the correct aspect ratio
-
-
-# Construct a string with glob to pass into the ffmpeg call
-
-
-# Render and write the MP4 movie using FFMPEG
-if (.Platform$OS.type == "unix"){
   
-  system(paste("ffmpeg -r ", frame_rate, " -pattern_type glob -i '",
-               dispersion_plot_glob, "' -c:v libx264 ", movie_output_name, ".mp4"),
-         sep = '')
-  
-}
-
 }
