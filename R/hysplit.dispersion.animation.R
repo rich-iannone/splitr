77c40103398d49c614e36ad7f9dfcc81f2e76163
rich-iannone <riannone@me.com>
2014-08-04 09:39:50 -0700

    Write 'particle.csv' file to a different location

--- R/hysplit.dispersion.animation.R
+++ R/hysplit.dispersion.animation.R
@@ -196,7 +196,7 @@ hysplit.dispersion.animation <- function(dispersion_df = NULL,
     
     if (.Platform$OS.type == "unix"){
       
-      write.table(particle_df, file = paste(df_folder_path, "/particle.csv", sep = ''),
+      write.table(particle_df, file = paste(path_output_files, "/particle.csv", sep = ''),
                   sep = ",", row.names = FALSE)
       
     }
