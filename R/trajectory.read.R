930b946fe71175007452ebccc2e66004fe604fac
rich-iannone <riannone@me.com>
2014-11-29 23:02:15 -0800

    Replace use of 'paste' with 'paste0'

--- R/trajectory.read.R
+++ R/trajectory.read.R
@@ -94,12 +94,12 @@ trajectory.read <- function(archive_folder,
                                                          ".*$"))
     } else if (!is.null(year) & !is.null(start_height_m_AGL)) {
       trajectory_file_list <- list.files(path = archive_folder,
-                                         pattern = paste("^traj.*?)-",
+                                         pattern = paste0("^traj.*?)-",
                                                          gsub("^[0-9][0-9]", "",
                                                               as.character(year)),
                                                          ".*?height_",
                                                          as.character(start_height_m_AGL),
-                                                         "-.*$", sep = ''))
+                                                         "-.*$"))
     }
     
   }
