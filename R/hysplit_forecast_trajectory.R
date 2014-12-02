78d545cb1f89bdb2a5e04cd3f31756de752b07a1
rich-iannone <riannone@me.com>
2014-12-02 00:05:06 -0800

    Replace use of 'paste' with 'paste0'

--- R/hysplit_forecast_trajectory.R
+++ R/hysplit_forecast_trajectory.R
@@ -60,10 +60,10 @@ hysplit_forecast_trajectory <- function(start_lat_deg,
     # If the most recent NAM forecast met file isn't already available, get the file
     today <- gsub("-", "", Sys.Date())
     
-    if (paste(path.expand(path_met_files), paste(today, ".t00z.namf", sep = ''),
-              sep = '') %in% list.files(path = gsub("/$", "", path_met_files),
-                                        full.names = TRUE) == FALSE) {
-      get.met.forecast.nam(path_met_files = path_met_files)
+    if (paste0(path.expand(path_met_files), paste0(today, ".t00z.namf")) %in%
+          list.files(path = gsub("/$", "", path_met_files),
+                     full.names = TRUE) == FALSE){
+      get_met_forecast_nam(path_met_files = path_met_files)
     }
     
     start_year_GMT <- year(Sys.Date())
@@ -77,76 +77,76 @@ hysplit_forecast_trajectory <- function(start_lat_deg,
     start_hour_GMT <- run_start_hour
     
     # Construct the output filename string for this model run
-    output_filename <- paste("traj-forecast",
-                             ifelse(backtrajectory == TRUE, '(back)', '(forward)'), "-",
-                             start_year_GMT, "-",
-                             start_month_GMT, "-",
-                             start_day_GMT, "-",
-                             start_hour_GMT, "-",
-                             "lat_", start_lat_deg, "_",
-                             "long_",start_long_deg, "-",
-                             "height_",start_height_m_AGL, "-",
-                             simulation_duration_h, "h", sep = '')
+    output_filename <- paste0("traj-forecast",
+                              ifelse(backtrajectory == TRUE, '(back)', '(forward)'), "-",
+                              start_year_GMT, "-",
+                              start_month_GMT, "-",
+                              start_day_GMT, "-",
+                              start_hour_GMT, "-",
+                              "lat_", start_lat_deg, "_",
+                              "long_",start_long_deg, "-",
+                              "height_",start_height_m_AGL, "-",
+                              simulation_duration_h, "h")
     
     # Write start year, month, day, hour to 'CONTROL'
     cat(start_year_GMT, " ", 
         start_month_GMT, " ",
         start_day_GMT, " ",
         start_hour_GMT, "\n",
-        file = paste(path_wd, "CONTROL", sep = ''),
+        file = paste0(path_wd, "CONTROL"),
         sep = '', append = FALSE)
     
     # Write number of starting locations to 'CONTROL'
     cat(no_starting_locations, "\n",
-        file = paste(path_wd, "CONTROL", sep = ''),
+        file = paste0(path_wd, "CONTROL"),
         sep = '', append = TRUE)
     
     # Write starting latitude, longitude, height AGL to 'CONTROL'
     cat(start_lat_deg, " ", 
         start_long_deg, " ", 
         start_height_m_AGL, "\n",
-        file = paste(path_wd, "CONTROL", sep = ''),
+        file = paste0(path_wd, "CONTROL"),
         sep = '', append = TRUE)
     
     # Write direction and number of simulation hours to 'CONTROL'
     cat(ifelse(backtrajectory == TRUE, "-", ""),
         simulation_duration_h, "\n",
-        file = paste(path_wd, "CONTROL", sep = ''),
+        file = paste0(path_wd, "CONTROL"),
         sep = '', append = TRUE)
     
     # Write vertical motion option to 'CONTROL'
     cat(vertical_motion_option, "\n",
-        file = paste(path_wd, "CONTROL", sep = ''),
+        file = paste0(path_wd, "CONTROL"),
         sep = '', append = TRUE)
     
     # Write top of model domain in meters to 'CONTROL'
     cat(top_of_model_domain_m, "\n",
-        file = paste(path_wd, "CONTROL", sep = ''),
+        file = paste0(path_wd, "CONTROL"),
         sep = '', append = TRUE)
     
     # Write number of met files used to 'CONTROL'
     cat("1", "\n",
-        file = paste(path_wd, "CONTROL", sep = ''),
+        file = paste0(path_wd, "CONTROL"),
         sep = '', append = TRUE)
     
     # Write met file path to 'CONTROL'
     cat(path_met_files, "\n",
-        file = paste(path_wd, "CONTROL", sep = ''),
+        file = paste0(path_wd, "CONTROL"),
         sep = '', append = TRUE)
     
     # Write file name for met file
-    cat(paste(today, ".t00z.namf", sep = ''), "\n",
-        file = paste(path_wd, "CONTROL", sep = ''),
+    cat(paste0(today, ".t00z.namf"), "\n",
+        file = paste0(path_wd, "CONTROL"),
         sep = '', append = TRUE)
     
     # Write path for trajectory output files to 'CONTROL'
     cat(path_wd, "\n",
-        file = paste(path_wd, "CONTROL", sep = ''),
+        file = paste0(path_wd, "CONTROL"),
         sep = '', append = TRUE)
     
     # Write name of output filename to 'CONTROL'
     cat(output_filename, "\n",
-        file = paste(path_wd, "CONTROL", sep = ''),
+        file = paste0(path_wd, "CONTROL"),
         sep = '', append = TRUE)
     
     # CONTROL file is now complete and in the working directory
@@ -154,19 +154,17 @@ hysplit_forecast_trajectory <- function(start_lat_deg,
     # the platform in win or unix
     
     if (.Platform$OS.type == "unix"){
-      system(paste("(cd ", path_wd, " && ", path_executable, "hyts_std)", sep = ''))
+      system(paste0("(cd ", path_wd, " && ", path_executable, "hyts_std)"))
       
       # Copy files from 'Working' to current project folder
-      system(paste("(cd ", path_wd, " && mv traj* '", path_output_files, "')", sep = ''))
+      system(paste0("(cd ", path_wd, " && mv traj* '", path_output_files, "')"))
     }
     
     if (.Platform$OS.type == "windows"){
-      shell(paste("(cd ", path_wd, " && ", path_executable, "hyts_std)", sep = ''))
+      shell(paste0("(cd ", path_wd, " && ", path_executable, "hyts_std)"))
       
       # Copy files from 'Working' to the output folder
-      #      shell(paste("(cd ", path_wd, " && move traj* '", path_output_files, "')", sep = ''))
-    }
-    
-  }
-  
+      #      shell(paste0("(cd ", path_wd, " && move traj* '", path_output_files, "')"))
+    } 
+  }  
 }
