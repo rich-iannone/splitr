5a614873860bc5c0939070f4987e0a5ea847621f
rich-iannone <riannone@me.com>
2014-12-03 11:27:02 -0800

    Modify function name

--- R/get_met_gdas1.R
+++ R/get_met_gdas1.R
@@ -4,9 +4,9 @@
 #' @param years a vector list of years for which GDAS1 files are to be obtained via FTP.
 #' @param months a vector list of months for which GDAS1 files are to be obtained via FTP. 
 #' @param path_met_files a full path should be provided for the location of the meteorological data files; downloaded files will be saved in this location.
-#' @export get.met.gdas1
+#' @export get_met_gdas1
 
-get.met.gdas1 <- function(files = NULL,
+get_met_gdas1 <- function(files = NULL,
                           years = NULL,
                           months = NULL,
                           path_met_files){
