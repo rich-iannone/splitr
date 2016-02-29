b2cfab31c55878e8c6f6635df6e4c6e2c7709741
Richard Iannone <riannone@me.com>
2016-02-28 22:29:21 -0800

    Refactor function

--- R/hysplit_dispersion_plot.R
+++ R/hysplit_dispersion_plot.R
@@ -1,19 +1,29 @@
 #' Plot HYSPLIT dispersion model output onto maps
-#' @description The function plots hourly outputs of dispersed particles onto maps.
-#' @param hours specification of hours of dispersion data to plot.
-#' @param dispersion_df optionally specify a data frame with dispersion data as an input.
-#' @param df_folder_path absolute path of the folder containing a dispersion data frame.
-#' @param map_type selection provider of base maps for plotting. Choices are 'osm' (Open Street Map) and 'stamen' (Stamen Maps).
-#' @param map_output_name a partial identifier prepended to the output map files.
-#' @param path_output_files a full path for a location that the dispersion output files were written.
+#' @description The function plots hourly outputs of
+#' dispersed particles onto maps.
+#' @param hours specification of hours of dispersion
+#' data to plot.
+#' @param dispersion_df optionally specify a data frame
+#' with dispersion data as an input.
+#' @param df_folder_path absolute path of the folder
+#' containing a dispersion data frame.
+#' @param map_type selection provider of base maps for
+#' plotting. Choices are 'osm' (Open Street Map) and
+#' 'stamen' (Stamen Maps).
+#' @param map_output_name a partial identifier
+#' prepended to the output map files.
+#' @param path_output_files a full path for a location
+#' that the dispersion output files were written.
 #' @export hysplit_dispersion_plot
 #' @examples
 #' \dontrun{
-#' # Make a set of hourly plots from a dispersion data frame
-#' hysplit_dispersion_plot(hours = "all",
-#'                         dispersion_df = disp.df,
-#'                         map_output_name = "new map",
-#'                         path_output_files = "~/Documents/SplitR/Output/Plots/")
+#' # Make a set of hourly plots from a dispersion
+#' # data frame
+#' hysplit_dispersion_plot(
+#'   hours = "all",
+#'   dispersion_df = disp.df,
+#'   map_output_name = "new map",
+#'   path_output_files = "~/Documents/SplitR/Output/Plots/")
 #'}
 
 hysplit_dispersion_plot <- function(hours = 'all',
@@ -23,60 +33,80 @@ hysplit_dispersion_plot <- function(hours = 'all',
                                     map_output_name = NULL,
                                     path_output_files = NULL){
   
-  # Obtain the appropriate dispersion data frame; if the value supplied to 'dispersion_df' is not
-  # null (and if a valid data frame object is given) use it as the dispersion_df
+  # Obtain the appropriate dispersion data frame; if
+  # the value supplied to 'dispersion_df' is not
+  # NULL (and if a valid data frame object is given)
+  # use it as the dispersion_df
   
   if (!is.null(dispersion_df)){
+    valid_names <- 
+      ifelse(all(c("particle_no", "lon", "lat", "height",
+                   "hour", "hour_start", "particle_id") %in%
+                   names(dispersion_df)), TRUE, FALSE)
     
-    valid_names <- ifelse(all(c("particle_no", "lon", "lat", "height",
-                                "hour", "hour_start", "particle_id") %in%
-                                names(dispersion_df)), TRUE, FALSE)
+    valid_classes <- 
+      ifelse(all(is.integer(dispersion_df[,1]) &
+                   is.numeric(dispersion_df[,2]) &
+                   is.numeric(dispersion_df[,3]) &
+                   is.numeric(dispersion_df[,4]) &
+                   is.numeric(dispersion_df[,5])),
+             TRUE, FALSE)
     
-    valid_classes <- ifelse(all(is.integer(dispersion_df[,1]) &
-                                  is.numeric(dispersion_df[,2]) &
-                                  is.numeric(dispersion_df[,3]) &
-                                  is.numeric(dispersion_df[,4]) &
-                                  is.numeric(dispersion_df[,5])), TRUE, FALSE)
-    
-    if (valid_names == FALSE | valid_classes == FALSE){ 
+    if (valid_names == FALSE |
+        valid_classes == FALSE){ 
       stop("The supplied data frame is not a valid dispersion df object.")
     }
   }
   
-  if (is.null(dispersion_df) & !is.null(df_folder_path)){
+  if (is.null(dispersion_df) &
+      !is.null(df_folder_path)){
     if (.Platform$OS.type == "unix"){
-      csv_absolute_path <- gsub("//", "/", paste(df_folder_path, "/dispersion.csv", sep = ''))
+      csv_absolute_path <- 
+        gsub("//", "/",
+             paste0(df_folder_path, "/dispersion.csv"))
     }
     
     if (.Platform$OS.type == "windows"){
       if (grepl("\\\\", df_folder_path)) df_folder_path <- gsub("\\\\", "", df_folder_path)
-      csv_absolute_path <- paste(df_folder_path, "\\dispersion.csv", sep = '')
+      csv_absolute_path <-
+        paste0(df_folder_path, "\\dispersion.csv")
     }
     
-    dispersion_df <- read.csv(csv_absolute_path,
-                              header = TRUE, stringsAsFactors = FALSE)
+    dispersion_df <- 
+      read.csv(csv_absolute_path,
+               header = TRUE,
+               stringsAsFactors = FALSE)
   }
   
-  # If value for 'hours' argument contains 'all' (default), determine the ending hour from
-  # the dispersion data frame
+  # If value for 'hours' argument contains 'all'
+  # (default), determine the ending hour from
+  # the dispersion dataframe
   if (hours == 'all'){
     last_hour <- max(dispersion_df$hour)
     hours <- 1:last_hour
   }
   
-  # If value for 'hours' argument contains a vector list, validate that vector to ensure
-  # those hours are within the range of hours in the dispersion data frame
+  # If value for 'hours' argument contains a vector
+  # list, validate that vector to ensure those hours
+  # are within the range of hours in the dispersion
+  # dataframe
   if (is.vector(hours)){
-    hours_dispersion_df <- unique(dispersion_df$hour)
-    hours <- hours[which(hours %in% hours_dispersion_df)]
+    hours_dispersion_df <-
+      unique(dispersion_df$hour)
+    hours <-
+      hours[which(hours %in% hours_dispersion_df)]
   }
   
   # Determine the extent of particle dispersion
-  bbox_data <- make_bbox(lon = dispersion_df$lon, lat = dispersion_df$lat)
+  bbox_data <- make_bbox(lon = dispersion_df$lon,
+                         lat = dispersion_df$lat)
   
-  # Create 'bounding_box' function to provide a square bounding box that's defined
-  # by the center-point lat/lon, and the distance away in kilometers
-  bounding_box <- function(lat, lon, dist, in.miles = TRUE){
+  # Create 'bounding_box' function to provide a
+  # square bounding box that's defined by the
+  # center-point lat/lon, and the distance away in
+  # kilometers
+  bounding_box <- function(lat, lon, dist,
+                           in.miles = TRUE){
     
     if (in.miles){
       ang_rad <- function(miles) miles/3958.756  
@@ -102,35 +132,42 @@ hysplit_dispersion_plot <- function(hours = 'all',
     m
   }
   
-  # Determine the distance away from the center-point to generate a bounding box
-  # for the map image that encompasses the bounding box for the dispersion data;
-  # this will keep generating new 'bbox_map' objects until the map extents are
-  # greater than the data extents
-  for (i in seq(from = 0.2, to = 1000, by = 0.2)){
-    
-    bbox_map <- bounding_box(lon = (bbox_data[[1]] + bbox_data[[3]])/2,
-                             lat = (bbox_data[[2]] + bbox_data[[4]])/2,
-                             i, in.miles = FALSE)
+  # Determine the distance away from the center-point
+  # to generate a bounding box for the map image that
+  # encompasses the bounding box for the dispersion
+  # data; this will keep generating new 'bbox_map'
+  # objects until the map extents are greater than the
+  # data extents
+  for (i in seq(from = 0.2,
+                to = 1000,
+                by = 0.2)){
+    bbox_map <- 
+      bounding_box(lon = (bbox_data[[1]] +
+                            bbox_data[[3]])/2,
+                   lat = (bbox_data[[2]] +
+                            bbox_data[[4]])/2,
+                   i, in.miles = FALSE)
     
     if (bbox_map[1] <= bbox_data[[1]] &
-          bbox_map[2] >= bbox_data[[3]] &
-          bbox_map[3] <= bbox_data[[2]] &
-          bbox_map[4] >= bbox_data[[4]]){
-      
+        bbox_map[2] >= bbox_data[[3]] &
+        bbox_map[3] <= bbox_data[[2]] &
+        bbox_map[4] >= bbox_data[[4]]){
       break()
     }
   }
   
-  # If chosen, a Stamen 'toner' style map that encompasses the bounds
-  # of the dispersion data will be downloaded
+  # If chosen, a Stamen 'toner' style map that
+  # encompasses the bounds of the dispersion data will
+  # be downloaded
   if (map_type == "stamen"){
     map <- get_map(location = bbox_map,
                    maptype = "toner",
                    source = "stamen")
   }
   
-  # If chosen, an Open Street Maps 'terrain' style map that encompasses
-  # the bounds of the dispersion data will be downloaded
+  # If chosen, an Open Street Maps 'terrain' style map
+  # that encompasses the bounds of the dispersion data
+  # will be downloaded
   if (map_type == "osm"){
     map <- get_map(location = bbox_map,
                    maptype = "terrain",
@@ -140,13 +177,16 @@ hysplit_dispersion_plot <- function(hours = 'all',
   for (i in hours){
     
     # Create a data frame that is a subset by hour
-    dispersion_df_hour <- subset(dispersion_df, hour == i)
+    dispersion_df_hour <-
+      subset(dispersion_df, hour == i)
     
     hour_x <- dispersion_df_hour$lon
     hour_y <- dispersion_df_hour$lat
     hour_h <- dispersion_df_hour$height
     
-    df_xyh <- as.data.frame(cbind(hour_x, hour_y, hour_h))
+    df_xyh <- as.data.frame(cbind(hour_x,
+                                  hour_y,
+                                  hour_h))
     
     # Remove vector objects from memory
     rm(hour_x, hour_y, hour_h)
