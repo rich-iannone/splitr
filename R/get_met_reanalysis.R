a91d2fdd8bdfcf8ee5f9f32820dd45c9667b39ec
Richard Iannone <riannone@me.com>
2016-02-28 20:03:24 -0800

    Add linebreaks and refactor URL construction

--- R/get_met_reanalysis.R
+++ R/get_met_reanalysis.R
@@ -12,75 +12,82 @@ get_met_reanalysis <- function(files = NULL,
   # Add require statements
   require(RCurl) 
   
+  reanalysis_dir <- 
+    "ftp://arlftp.arlhq.noaa.gov/archives/reanalysis/"
+  
   # Download list of reanalysis met files by name
   if (!is.null(files)){
     
     for (i in 1:length(files)){
       
       if (.Platform$OS.type == "windows"){
-        download.file(url = paste0("ftp://arlftp.arlhq.noaa.gov/archives/reanalysis/",
-                                  files[i]),
-                      destfile = paste0(path_met_files,
-                                       "\\", files[i]),
-                      method = "auto",
-                      quiet = FALSE,
-                      mode = "wb",
-                      cacheOK = FALSE)
+        download.file(
+          url = paste0(reanalysis_dir,
+                       files[i]),
+          destfile = paste0(path_met_files,
+                            "\\", files[i]),
+          method = "auto",
+          quiet = FALSE,
+          mode = "wb",
+          cacheOK = FALSE)
       }
       
       if (.Platform$OS.type == "unix"){
-        
-        download.file(url = paste0("ftp://arlftp.arlhq.noaa.gov/archives/reanalysis/",
-                                  files[i]),
-                      destfile = paste0(path_met_files,
-                                       files[i]),
-                      method = "auto",
-                      quiet = FALSE,
-                      mode = "wb",
-                      cacheOK = FALSE) 
+        download.file(
+          url = paste0(reanalysis_dir,
+                       files[i]),
+          destfile = paste0(path_met_files,
+                            files[i]),
+          method = "auto",
+          quiet = FALSE,
+          mode = "wb",
+          cacheOK = FALSE) 
       } 
     }
   }
   
   # Download one or more years of reanalysis met files
   if (!is.null(years)){
-    
     for (i in 1:length(years)){
-      
       for (j in 1:12){
-        
         if (.Platform$OS.type == "unix"){
-          
-          download.file(url = paste0("ftp://arlftp.arlhq.noaa.gov/archives/reanalysis/RP",
-                                    years[i],
-                                    formatC(j, width = 2, format = "d", flag = "0"),
-                                    ".gbl"),
-                        destfile = paste0(path_met_files,
-                                         "RP",
-                                         years[i],
-                                         formatC(j, width = 2, format = "d", flag = "0"),
-                                         ".gbl"),
-                        method = "auto",
-                        quiet = FALSE,
-                        mode = "wb",
-                        cacheOK = FALSE)
+          download.file(
+            url = paste0(reanalysis_dir,
+                         "RP",
+                         years[i],
+                         formatC(j, width = 2,
+                                 format = "d", flag = "0"),
+                         ".gbl"),
+            destfile = paste0(path_met_files,
+                              "RP",
+                              years[i],
+                              formatC(j, width = 2,
+                                      format = "d", flag = "0"),
+                              ".gbl"),
+            method = "auto",
+            quiet = FALSE,
+            mode = "wb",
+            cacheOK = FALSE)
         }
         
         if (.Platform$OS.type == "windows"){
-          
-          download.file(url = paste0("ftp://arlftp.arlhq.noaa.gov/archives/reanalysis/RP",
-                                    years[i],
-                                    formatC(j, width = 2, format = "d", flag = "0"),
-                                    ".gbl"),
-                        destfile = paste0(path_met_files, "//",
-                                         "RP",
-                                         years[i],
-                                         formatC(j, width = 2, format = "d", flag = "0"),
-                                         ".gbl"),
-                        method = "auto",
-                        quiet = FALSE,
-                        mode = "wb",
-                        cacheOK = FALSE)
+          download.file(
+            url = paste0(reanalysis_dir,
+                         "RP",
+                         years[i],
+                         formatC(j, width = 2,
+                                 format = "d", flag = "0"),
+                         ".gbl"),
+            destfile = paste0(path_met_files, "//",
+                              "RP",
+                              years[i],
+                              formatC(j, width = 2,
+                                      format = "d", flag = "0"),
+                              ".gbl"),
+            method = "auto",
+            quiet = FALSE,
+            mode = "wb",
+            cacheOK = FALSE)
         }
       }
     }
