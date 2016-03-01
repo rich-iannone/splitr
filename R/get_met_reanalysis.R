7771a2cbf3a6330e0349208ed9b728d8969e8ef3
Richard Iannone <riannone@me.com>
2016-03-01 01:42:05 -0800

    Update roxygen documentation

--- R/get_met_reanalysis.R
+++ R/get_met_reanalysis.R
@@ -3,8 +3,8 @@
 #' meteorology data files from the NOAA FTP server and
 #' saves them to a specified folder. Files can be
 #' downloaded either by specifying a list of filenames
-#' (in the form of "RP[YYYY][MM].gbl") or through bulk
-#' download of a year of files.
+#' (in the form of \code{'RP[YYYY][MM].gbl'}) or
+#' through bulk download of a year of files.
 #' @param files a vector list of exact filenames for
 #' the reanalysis files.
 #' @param years a vector list of years for which
