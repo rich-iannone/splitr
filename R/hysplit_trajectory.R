cf63e5b36942eee87d360fbf1196d247f57099e5
Richard Iannone <riannone@me.com>
2016-03-05 23:23:37 -0800

    Modify paths for working directory

--- R/hysplit_trajectory.R
+++ R/hysplit_trajectory.R
@@ -530,13 +530,13 @@ hysplit_trajectory <- function(traj_name = NULL,
             start_month_GMT, " ",
             start_day_GMT, " ",
             start_hour_GMT, "\n",
-            file = paste0(path_wd, "\\", "CONTROL"),
+            file = paste0(getwd(), "\\", "CONTROL"),
             sep = '', append = FALSE)
         
         # Write number of starting locations
         # to 'CONTROL'
         cat("1\n",
-            file = paste0(path_wd, "\\", "CONTROL"),
+            file = paste0(getwd(), "\\", "CONTROL"),
             sep = '', append = TRUE)
         
         # Write starting latitude, longitude, height
@@ -544,47 +544,47 @@ hysplit_trajectory <- function(traj_name = NULL,
         cat(start_lat_deg, " ", 
             start_long_deg, " ", 
             start_height_m_AGL, "\n",
-            file = paste0(path_wd, "\\", "CONTROL"),
+            file = paste0(getwd(), "\\", "CONTROL"),
             sep = '', append = TRUE)
         
         # Write direction and number of simulation
         # hours to 'CONTROL'
         cat(ifelse(backtrajectory == TRUE, "-", ""),
             simulation_duration_h, "\n",
-            file = paste0(path_wd, "\\", "CONTROL"),
+            file = paste0(getwd(), "\\", "CONTROL"),
             sep = '', append = TRUE)
         
         # Write vertical motion option to 'CONTROL'
         cat(vertical_motion_option, "\n",
-            file = paste0(path_wd, "\\", "CONTROL"),
+            file = paste0(getwd(), "\\", "CONTROL"),
             sep = '', append = TRUE)
         
         # Write top of model domain in meters to
         # 'CONTROL'
         cat(top_of_model_domain_m, "\n",
-            file = paste0(path_wd, "\\", "CONTROL"),
+            file = paste0(getwd(), "\\", "CONTROL"),
             sep = '', append = TRUE)
         
         # Write number of met files used to 'CONTROL'
         cat(length(met), "\n",
-            file = paste0(path_wd, "\\", "CONTROL"),
+            file = paste0(getwd(), "\\", "CONTROL"),
             sep = '', append = TRUE)
         
         # Write met file paths to 'CONTROL'
         for (i in 1:length(met)){
           cat(path_met_files, "\n", met[i], "\n",
-              file = paste0(path_wd, "\\", "CONTROL"),
+              file = paste0(getwd(), "\\", "CONTROL"),
               sep = '', append = TRUE)}
         
         # Write path for trajectory output files to
         # 'CONTROL'
-        cat(path_wd, "\n",
-            file = paste0(path_wd, "\\", "CONTROL"),
+        cat(getwd(), "\n",
+            file = paste0(getwd(), "\\", "CONTROL"),
             sep = '', append = TRUE)
         
         # Write name of output filename to 'CONTROL'
         cat(output_filename, "\n",
-            file = paste0(path_wd, "\\", "CONTROL"),
+            file = paste0(getwd(), "\\", "CONTROL"),
             sep = '', append = TRUE)
       }
       
