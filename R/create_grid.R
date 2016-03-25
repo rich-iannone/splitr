405a1f080fb871dea3b9853a448d710262f3f3f6
Richard Iannone <riannone@me.com>
2016-03-25 00:38:43 -0700

    Update all roxygen @param statements

--- R/create_grid.R
+++ R/create_grid.R
@@ -1,14 +1,24 @@
 #' Create a grid of sites
 #' @description Flexibly create a grid of sites for
 #' ensemble trajectory model runs.
-#' @param lat_deg 
-#' @param lon_deg
-#' @param grid_ref
-#' @param n_s_dist
-#' @param w_e_dist
-#' @param n_s_division
-#' @param w_e_division
-#' @param dist_units
+#' @param lat_deg a latitude value in decimal degrees
+#' for the point of reference on the grid.
+#' @param lon_deg a longitude value in decimal degrees
+#' for the point of reference on the grid.
+#' @param grid_ref the grid reference point. The
+#' default value is \code{center}.
+#' @param n_s_dist the north-to-south distance of the
+#' grid.
+#' @param w_e_dist the west-to-east distance of the
+#' grid.
+#' @param n_s_division the north-to-south distances of
+#' the those divisions in the grid.
+#' @param w_e_division the west-to-east distances of
+#' the those divisions in the grid.
+#' @param dist_units the distance units used with
+#' values supplied to  \code{n_s_dist},
+#' \code{w_e_dist}, \code{n_s_division}, and
+#' \code{w_e_division}. The default is \code{degrees}.
 #' @export create_grid
 
 create_grid <- function(lat_deg = 49.263,
