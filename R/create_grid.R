bccddef5c152a6d72cee7f8a7644f5c6732c9d63
Richard Iannone <riannone@me.com>
2016-03-24 23:57:49 -0700

    Add function 'create_grid'

---
+++ R/create_grid.R
@@ -0,0 +1,24 @@
+#' Create a grid of sites
+#' @description Flexibly create a grid of sites for
+#' ensemble trajectory model runs.
+#' @param lat_deg 
+#' @param lon_deg
+#' @param grid_ref
+#' @param w_e_dist
+#' @param n_s_dist
+#' @param w_e_division
+#' @param n_s_division
+#' @param dist_units
+#' @export create_grid
+
+create_grid <- function(lat_deg = 49.196116,
+                        lon_deg = -122.505866,
+                        grid_ref = "center",
+                        w_e_dist = 0.5,
+                        n_s_dist = 0.5,
+                        w_e_division = 0.1,
+                        n_s_division = 0.1,
+                        dist_units = "degrees",){
+  
+  
+}
