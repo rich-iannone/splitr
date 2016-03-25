ca0df42e3594e97c98115904c7235236cd4e84ab
Richard Iannone <riannone@me.com>
2016-03-25 00:03:38 -0700

    Modify order of arguments

--- R/create_grid.R
+++ R/create_grid.R
@@ -4,21 +4,22 @@
 #' @param lat_deg 
 #' @param lon_deg
 #' @param grid_ref
-#' @param w_e_dist
 #' @param n_s_dist
-#' @param w_e_division
+#' @param w_e_dist
 #' @param n_s_division
+#' @param w_e_division
 #' @param dist_units
 #' @export create_grid
 
 create_grid <- function(lat_deg = 49.196116,
                         lon_deg = -122.505866,
                         grid_ref = "center",
-                        w_e_dist = 0.5,
                         n_s_dist = 0.5,
-                        w_e_division = 0.1,
+                        w_e_dist = 0.5,
                         n_s_division = 0.1,
+                        w_e_division = 0.1,
                         dist_units = "degrees"){
   
   
+
 }
