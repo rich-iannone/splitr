44eb25072405833028fbb4c7ef77d304b2dbd476
Richard Iannone <riannone@me.com>
2016-03-29 19:13:40 -0700

    Shorten several object names

--- R/create_grid.R
+++ R/create_grid.R
@@ -1,9 +1,9 @@
 #' Create a grid of sites
 #' @description Flexibly create a grid of sites for
 #' ensemble trajectory model runs.
-#' @param lat_deg a latitude value in decimal degrees
+#' @param lat a latitude value in decimal degrees
 #' for the point of reference on the grid.
-#' @param lon_deg a longitude value in decimal degrees
+#' @param lon a longitude value in decimal degrees
 #' for the point of reference on the grid.
 #' @param grid_ref the grid reference point. The
 #' default value is \code{center}.
@@ -21,8 +21,8 @@
 #' \code{w_e_division}. The default is \code{degrees}.
 #' @export create_grid
 
-create_grid <- function(lat_deg = 49.263,
-                        lon_deg = -123.250,
+create_grid <- function(lat = 49.263,
+                        lon = -123.250,
                         grid_ref = "center",
                         n_s_dist = 5,
                         w_e_dist = 5,
@@ -33,20 +33,21 @@ create_grid <- function(lat_deg = 49.263,
   n_s_points <- n_s_dist / n_s_division
   w_e_points <- w_e_dist / w_e_division
   
-  lat <- (lat_deg + n_s_dist/2) - seq(0, n_s_dist, n_s_division)
-  lon <- (lon_deg + w_e_dist/2) - seq(0, w_e_dist, w_e_division)
+  lat_vec <- sort((lat + n_s_dist/2) - seq(0, n_s_dist, n_s_division),
+                  decreasing = TRUE)
+  lon_vec <- sort((lon + w_e_dist/2) - seq(0, w_e_dist, w_e_division))
   
-  for (i in 1:length(lat)){
-    for (j in 1:length(lon)){
-      if (i == 1){
+  for (i in 1:length(lat_vec)){
+    for (j in 1:length(lon_vec)){
+      if (i == 1 & j == 1){
         coords <- vector("list", 2)
         names(coords) <- c("lat", "lon")
       }
-
-      coords$lat[length(coords$lat) + 1] <- lat[i]
-      coords$lon[length(coords$lon) + 1] <- lon[j]
+      
+      coords$lat[length(coords$lat) + 1] <- lat_vec[i]
+      coords$lon[length(coords$lon) + 1] <- lon_vec[j]
     }
   }
-
+  
   return(coords)
 }
