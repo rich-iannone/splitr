40f7edb40a19d6c75a2f7619272766194c62e6f3
Richard Iannone <riannone@me.com>
2016-04-01 09:13:23 -0700

    Add function 'get_traj_df'

---
+++ R/get_traj_df.R
@@ -0,0 +1,17 @@
+#' Get the trajectory data frame from a model object
+#' @description If a data frame is available within a 
+#' model object, get the entire data frame or a subset
+#' of it
+#' @param model a SplitR modeling object
+#' @export get_traj_df
+
+get_traj_df <- function(model){
+
+  if (is.null){
+    return(NA)
+  }
+  
+  if (!is.null(model$traj_df)){
+    return(traj_df)
+  }
+}
