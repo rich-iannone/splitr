45399d1aadd245a4e6f613bc0636b9b61c04e3e9
Richard Iannone <riannone@me.com>
2016-04-02 08:53:40 -0700

    Add function 'create_disp_model'

---
+++ R/create_disp_model.R
@@ -0,0 +1,33 @@
+#' Create a dispersion model
+#' @description Create a dispersion model object to
+#' begin a modelling pipeline.
+#' @param name an optional name for the dispersion
+#' model object.
+#' @export create_disp_model
+
+create_disp_model <- function(name = NULL){
+  
+  # Create the 'disp_model' list object
+  disp_model <- 
+    list(disp_name = NULL,
+         lat = NULL,
+         lon = NULL,
+         height = NULL,
+         duration = NULL,
+         start_day = NULL,
+         start_hour = NULL,
+         backward_running = FALSE,
+         met_type = NULL,
+         emissions = NULL,
+         species = NULL,
+         grids = NULL,
+         vert_motion = 0,
+         model_height = 20000,
+         disp_df = NULL)
+  
+  attr(disp_model, "class") <- "disp_model"
+  
+  if (!is.null(name)) disp_model$disp_name <- name
+  
+  return(disp_model)
+}
