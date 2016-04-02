330dbc975e09714b52f3b4180bcca1a73efc10f6
Richard Iannone <riannone@me.com>
2016-04-01 17:32:54 -0700

    Simplify arguments in function

--- R/add_grid.R
+++ R/add_grid.R
@@ -2,16 +2,27 @@
 #' @description Create and add a grid of latitude and
 #' longitude points to a model object
 #' @param model a SplitR modeling object
+#' @param lat a latitude value in decimal degrees
+#' for the point of reference on the grid.
+#' @param lon a longitude value in decimal degrees
+#' for the point of reference on the grid.
+#' @param grid_ref the grid reference point. The
+#' default value is \code{center}.
+#' @param range the latitude and longitude range about
+#' the \code{grid_ref}.
+#' @param division the division distances across the
+#' latitude and longitude ranges.
+#' @param dist_units the distance units used with
+#' values supplied to \code{range} and
+#' \code{division}. The default is \code{degrees}.
 #' @export add_grid
 
 add_grid <- function(model,
                      lat = 49.263,
                      lon = -123.250,
                      grid_ref = "center",
-                     n_s_dist = 5,
-                     w_e_dist = 5,
-                     n_s_division = 0.5,
-                     w_e_division = 0.5,
+                     range = c(5, 5),
+                     division = c(0.5, 0.5),
                      dist_units = "degrees"){
  
   # Obtain the grid of lat/lon points
@@ -20,10 +31,8 @@ add_grid <- function(model,
       lat = lat,
       lon = lon,
       grid_ref = grid_ref,
-      n_s_dist = n_s_dist,
-      w_e_dist = w_e_dist,
-      n_s_division = n_s_division,
-      w_e_division = w_e_division,
+      range = range,
+      division = division,
       dist_units = dist_units)
   
   # Add the grid points to the model object
