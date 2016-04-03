72db3ac3d4e67c2e0b961d67ffe7ae9869a81cc0
Richard Iannone <riannone@me.com>
2016-04-03 00:25:29 -0700

    Create function 'add_emissions'

---
+++ R/add_emissions.R
@@ -0,0 +1,69 @@
+#' Add emissions parameters to a dispersion model
+#' @description Add a set of emissions parameters to
+#' a dispersion model object. Multiple sets of
+#' emissions parameters can can be added to a single
+#' dispersion model object.
+#' @param model a SplitR modeling object.
+#' @param name an identifier for the group of emissions
+#' parameters.
+#' @param rate the rate of emissions for the pollutant
+#' in mass units per hour.
+#' @param duration the duration of emissions in hours.
+#' @param start_day the day that the emissions will 
+#' begin. This should take the form of a
+#' single-length vector for a day (\code{"YYYY-MM-DD"}).
+#' @param start_hour a single daily hour as an
+#' integer hour (from \code{0} to \code{23}).
+#' @export add_emissions
+
+add_emissions <- function(model,
+                          name = NULL,
+                          rate = NULL,
+                          duration = NULL,
+                          start_day = NULL,
+                          start_hour = NULL){
+  
+  if (is.null(name)){
+    if (is.null(model$emissions)){
+      name <- "emissions_1"
+    } else {
+      name <- paste0("emissions_",
+                     nrow(model$emissions) + 1)
+    }
+  }
+  
+  if (is.null(rate)){
+    rate <- 1
+  }
+  
+  if (is.null(duration)){
+    duration <- 1
+  }
+  
+  if (is.null(start_day)){
+    start_day <- "10-05-01"
+  }
+  
+  if (is.null(start_hour)){
+    start_hour <- 0
+  }
+  
+  # Write emissions parameters to a data frame
+  emissions <- 
+    data.frame(
+      name = name,
+      rate = rate,
+      duration = duration,
+      start_day = start_day,
+      start_hour = start_hour,
+      stringsAsFactors = FALSE)
+  
+  # Write data frame to the `emissions` list
+  # component of `model`
+  if (is.null(model$emissions)){
+    model$emissions <- emissions
+  } else {
+    model$emissions <- 
+      rbind(model$emissions, emissions)
+  }
+}
