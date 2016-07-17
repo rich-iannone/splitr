6d79778a7701f212d6170bd32be639638f0a2b87
Richard Iannone <riannone@me.com>
2016-07-16 21:35:49 -0700

    Add to @import statement

--- R/get_met_forecast_nam.R
+++ R/get_met_forecast_nam.R
@@ -5,30 +5,40 @@
 #' @param path_met_files a full path should be provided
 #' for the location of the meteorological data files;
 #' downloaded files will be saved in this location.
-#' @import downloader
+#' @import downloader RCurl
 #' @export get_met_forecast_nam
 
 get_met_forecast_nam <- function(path_met_files) {
   
-  # Establish which forecast dirs are currently available on the server
+  # Establish which forecast dirs are currently 
+  # available on the server
   forecast_dirs <-
     grep("^[0-9].*$",
-         unlist(strsplit(getURL("ftp://arlftp.arlhq.noaa.gov/forecast/",
-                                dirlistonly = TRUE), "\n")), value = TRUE)
+         unlist(
+           strsplit(
+             RCurl::getURL(
+               "ftp://arlftp.arlhq.noaa.gov/forecast/",
+               dirlistonly = TRUE), "\n")), 
+         value = TRUE)
   
-  # Get today's date and write in format equivalent to FTP directories 
+  # Get today's date and write in format equivalent
+  # to FTP directories 
   today <- gsub("-", "", Sys.Date())
   
-  # Download today's namf file
-  # -- CONUS, 12 km, 3 hrly, pressure levels, 48 h forecast
+  # Download today's `namf`` file
+  # -- CONUS, 12 km, 3 hrly, pressure levels,
+  #    48 h forecast
   
   if (today %in% forecast_dirs) {
-    download(url = paste0("ftp://arlftp.arlhq.noaa.gov/forecast/",
-                          today, "/hysplit.t00z.namf"),
-             destfile = paste0(path_met_files, paste0(today, ".t00z.namf")),
-             method = "auto",
-             quiet = FALSE,
-             mode = "w",
-             cacheOK = TRUE)
+    download(
+      url = paste0(
+        "ftp://arlftp.arlhq.noaa.gov/forecast/",
+        today, "/hysplit.t00z.namf"),
+      destfile = paste0(path_met_files, 
+                        paste0(today, ".t00z.namf")),
+      method = "auto",
+      quiet = FALSE,
+      mode = "w",
+      cacheOK = TRUE)
   } 
 }
