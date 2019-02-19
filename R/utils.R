3261ba8ffdd24dfb8b448e208a5f591afba50025
Richard Iannone <riannone@me.com>
2019-02-18 23:47:05 -0500

    Let `met_download()` accept multiple URLs

--- R/utils.R
+++ R/utils.R
@@ -1,19 +1,22 @@
 
 #' Download files using consistent options
-#' @param url The URL from which the files reside.
+#' @param urls One or more URLs from which the files reside.
 #' @param local_path The path to which the files should be written.
 #' @noRd
-met_download <- function(url,
+met_download <- function(urls,
                          local_path) {
   
-  download(
-    url = url,
-    destfile = local_path,
-    method = "auto",
-    quiet = TRUE,
-    mode = "wb",
-    cacheOK = FALSE
-  )
+  for (url in urls) {
+    
+    download(
+      url = url,
+      destfile = local_path,
+      method = "auto",
+      quiet = TRUE,
+      mode = "wb",
+      cacheOK = FALSE
+    )
+  }
 }
 
 #' Read a `listing` file
