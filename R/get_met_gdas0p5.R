044aaf24f39ecb4bc28a4c658ad937f3dedeaf4a
Richard Iannone <riannone@me.com>
2019-02-19 00:31:50 -0500

    Refactor `get_met_gdas0p5()`

--- R/get_met_gdas0p5.R
+++ R/get_met_gdas0p5.R
@@ -14,7 +14,6 @@
 #' @param path_met_files a full path should be provided for the location of the
 #'   meteorological data files; downloaded files will be saved in this location.
 #' @import downloader
-#' @import lubridate
 #' @export
 get_met_gdas0p5 <- function(files = NULL,
                             years = NULL,
@@ -22,572 +21,119 @@ get_met_gdas0p5 <- function(files = NULL,
                             days = NULL,
                             path_met_files) {
   
-  gdas0p5_dir <- "ftp://arlftp.arlhq.noaa.gov/archives/gdas0p5/"
+  gdas0p5_dir <- "ftp://arlftp.arlhq.noaa.gov/archives/gdas0p5"
+  
+  url_of_listing <- file.path(gdas0p5_dir, "listing")
   
   # Download the 'listing' file from NOAA server
   # It contains a list of GDAS0p5 files currently
   # available on the server
-  download(
-    url = paste0(gdas0p5_dir, "listing"),
-    destfile = paste0(path_met_files, "listing"),
-    method = "auto",
-    quiet = TRUE,
-    mode = "wb",
-    cacheOK = FALSE)
+  met_download(
+    urls = url_of_listing,
+    local_path = file.path(path_met_files, "listing")
+  )
   
-  # Create object 'listing' as a vector with all filenames available on the FTP server
-  listing <- 
-    as.vector(
-      read.table(
-        paste0(path_met_files, "listing"),
-        sep = "\n"
-      )[,1]
-    )
+  # Create object 'listing' as a vector with all
+  # filenames available on the FTP server
+  listing <- read_listing_file(url_of_listing)
   
   # Download list of GDAS0p5 met files by name
   if (!is.null(files)) {
     
-    # Determine whether all of the requested files are available on the server
-    files_available <- ifelse(all(files %in% listing), TRUE, FALSE)
-    
-    # If all requested files are available on the server, download those files
-    if (files_available == TRUE) {
+    # If not all files available on the server,
+    # provide a warning
+    if (!all(files %in% listing)) {
       
-      for (i in 1:length(files)) {
-        
-        download(
-          url = paste0(gdas0p5_dir, files[i]),
-          destfile = paste0(path_met_files, files[i]),
-          method = "auto",
-          quiet = FALSE,
-          mode = "wb",
-          cacheOK = FALSE
-        )
-      }
+      warning("Not all files requested are available on the server:\n",
+              " * ", paste(base::setdiff(files, listing), collapse = ", "),
+              " not available.", call. = FALSE)
     }
+    
+    # Get a vector of the available files
+    files_available <- base::intersect(listing, files)
+    
+    # Download the meteorology data files
+    met_download(
+      urls = file.path(gdas0p5_dir, files_available),
+      local_path = file.path(path_met_files, files_available)
+    )
   }
   
   # Download list of GDAS0p5 met files for one or more years
   if (!is.null(years)) {
     
-    # Create a list of filenames to compare with those currently available
-    # on the NOAA FTP server
-    for (i in 1:length(years)) {
-      
-      if (i == 1) the_list <- vector(mode = "character", length = 0)
-      
-      jan <- seq(1, 31, 1)
-      
-      for (j in 1:length(jan)) {
-        the_list <- 
-          c(the_list, 
-            paste0(years[i], "01",
-                   formatC(j, width = 2, format = "d", flag = "0"),
-                   "_gdas0p5"))
-      }
-      
-      if (years[i] %% 4 != 0) feb <- seq(1, 29, 1)
-      if (years[i] %% 4 == 0) feb <- seq(1, 28, 1)
-      
-      for (j in 1:length(feb)) {
-        the_list <- 
-          c(the_list, 
-            paste0(years[i], "02",
-                   formatC(j, width = 2, format = "d", flag = "0"),
-                   "_gdas0p5"))
-      }
-      
-      mar <- seq(1, 31, 1)
-      
-      for (j in 1:length(mar)) {
-        the_list <- 
-          c(the_list, 
-            paste0(years[i], "03",
-                   formatC(j, width = 2, format = "d", flag = "0"),
-                   "_gdas0p5"))
-      }
-      
-      apr <- seq(1, 30, 1)
-      
-      for (j in 1:length(apr)) {
-        the_list <- 
-          c(the_list, 
-            paste0(years[i], "04",
-                   formatC(j, width = 2, format = "d", flag = "0"),
-                   "_gdas0p5"))
-      }
-      
-      may <- seq(1, 31, 1)
-      
-      for (j in 1:length(may)) {
-        the_list <- 
-          c(the_list, 
-            paste0(years[i], "05",
-                   formatC(j, width = 2, format = "d", flag = "0"),
-                   "_gdas0p5"))
-      }
-      
-      jun <- seq(1, 30, 1)
-      
-      for (j in 1:length(jun)) {
-        the_list <- 
-          c(the_list, 
-            paste0(years[i], "06",
-                   formatC(j, width = 2, format = "d", flag = "0"),
-                   "_gdas0p5"))
-      }
-      
-      jul <- seq(1, 31, 1)
-      
-      for (j in 1:length(jul)) {
-        the_list <- 
-          c(the_list, 
-            paste0(years[i], "07",
-                   formatC(j, width = 2, format = "d", flag = "0"),
-                   "_gdas0p5"))
-      }
-      
-      aug <- seq(1, 31, 1)
-      
-      for (j in 1:length(aug)) {
-        the_list <- 
-          c(the_list, 
-            paste0(years[i], "08",
-                   formatC(j, width = 2, format = "d", flag = "0"),
-                   "_gdas0p5"))
-      }
-      
-      sep <- seq(1, 30, 1)
-      
-      for (j in 1:length(sep)) {
-        the_list <- 
-          c(the_list, 
-            paste0(years[i], "09",
-                   formatC(j, width = 2, format = "d", flag = "0"),
-                   "_gdas0p5"))
-      }
-      
-      oct <- seq(1, 31, 1)
-      
-      for (j in 1:length(oct)) {
-        the_list <- 
-          c(the_list, 
-            paste0(years[i], "10",
-                   formatC(j, width = 2, format = "d", flag = "0"),
-                   "_gdas0p5"))
-      }
-      
-      nov <- seq(1, 30, 1)
-      
-      for (j in 1:length(nov)) {
-        the_list <- 
-          c(the_list, 
-            paste0(years[i], "11",
-                   formatC(j, width = 2, format = "d", flag = "0"),
-                   "_gdas0p5"))
-      }
-      
-      dec <- seq(1, 31, 1)
-      
-      for (j in 1:length(dec)) {
-        the_list <- 
-          c(the_list, 
-            paste0(years[i], "12",
-                   formatC(j, width = 2, format = "d", flag = "0"),
-                   "_gdas0p5"))
-      }
+    listing_years <- 
+      listing %>%
+      substring(1, 4)
+    
+    if (is.numeric(years)) {
+      years <- as.character(years)
     }
     
-    # Determine whether all of the requested files are available on the server
-    all_files_available <- ifelse(all(the_list %in% listing), TRUE, FALSE)
+    # Get a vector of the available files
+    files_available <- listing[listing_years %in% years]
     
-    # Determine which files are missing
-    if (all_files_available == FALSE) {
-      for (i in 1:length(the_list)) {
-        if (i == 1) files_missing <- vector(mode = "character", length = 0)
-        if (the_list[i] %in% listing) {
-          NULL
-        } else {
-          files_missing <- c(files_missing, the_list[i])
-        }
-      }
-      
-      # Generate report of missing files
-      missing_report <- 
-        as.data.frame(mat.or.vec(nr = length(files_missing), nc = 2))
-      
-      colnames(missing_report) <- c("filename", "date")
-      
-      # Add names of missing files to the missing file report
-      missing_report$filename <- files_missing
-      
-      # Get dates for missing files
-      for (i in 1:length(files_missing)) {
-        if (i == 1) dates <- mat.or.vec(nr = length(files_missing), nc = 1)
-        dates[i] <- as.character(ymd(gsub("^([0-9]*).*", "\\1", files_missing[i])))
-      }
-      
-      # Add dates of missing files to the missing file report
-      missing_report$date <- dates
-      
-      # Filter list of files to obtain
-      the_list <- setdiff(the_list, files_missing)
-    }
-    
-    # Download the requested files from the server
-    for (i in 1:length(the_list)) {
-      
-      download(
-        url = paste0(gdas0p5_dir,the_list[i]),
-        destfile = paste0(path_met_files, the_list[i]),
-        method = "auto",
-        quiet = FALSE,
-        mode = "wb",
-        cacheOK = FALSE
-      )
-    }
-    
-    # Write file that contains report of missing files
-    if (exists("missing_report")) {
-      
-      write.table(
-        missing_report,
-        file = paste0(
-          path_met_files,
-          "Missing GDAS 0.5 Files - ", 
-          format(Sys.time(), "%B %d %Y %H_%M_%S")
-        ),
-        quote = FALSE, row.names = FALSE)
-    }
+    # Download the meteorology data files
+    met_download(
+      urls = file.path(gdas0p5_dir, files_available),
+      local_path = file.path(path_met_files, files_available)
+    )
   }
   
   # Download list of GDAS0p5 met files for one or more months
   if (!is.null(months)) {
     
-    # Create a list of filenames to compare with those currently available
-    # on the NOAA FTP server
-    for (i in 1:length(months)) {
+    listing_months <- 
+      listing %>%
+      substring(5, 6)
+    
+    if (is.numeric(months)) {
       
-      if (i == 1) the_list <- vector(mode = "character", length = 0)
-      
-      the_month <- as.numeric(gsub("^([0-9]*).*", "\\1", months[i]))
-      the_year <- as.numeric(gsub("^[0-9][0-9]-([0-9]*)", "\\1", months[i]))
-      
-      if (the_month == 1) {
-        jan <- seq(1, 31, 1)
-        for (j in 1:length(jan)) {
-          the_list <- 
-            c(the_list, 
-              paste0(the_year, "01",
-                     formatC(j, width = 2, format = "d", flag = "0"),
-                     "_gdas0p5"))
-        }
-      }
-      
-      if (the_month == 2) {
-        if (the_year %% 4 != 0) feb <- seq(1, 29, 1)
-        if (the_year %% 4 == 0) feb <- seq(1, 28, 1)
-        for (j in 1:length(feb)) {
-          the_list <- 
-            c(the_list, 
-              paste0(the_year, "02",
-                     formatC(j, width = 2, format = "d", flag = "0"),
-                     "_gdas0p5"))
-        }
-      }
-      
-      if (the_month == 3) {
-        mar <- seq(1, 31, 1)
-        for (j in 1:length(mar)) {
-          the_list <- 
-            c(the_list, 
-              paste0(the_year, "03",
-                     formatC(j, width = 2, format = "d", flag = "0"),
-                     "_gdas0p5"))
-        }
-      }
-      
-      if (the_month == 4) {
-        apr <- seq(1, 30, 1)
-        for (j in 1:length(apr)) {
-          the_list <- 
-            c(the_list,
-              paste0(the_year, "04",
-                     formatC(j, width = 2, format = "d", flag = "0"),
-                     "_gdas0p5"))
-        }
-      }
-      
-      if (the_month == 5) {
-        may <- seq(1, 31, 1)
-        for (j in 1:length(may)) {
-          the_list <- 
-            c(the_list,
-              paste0(the_year, "05",
-                     formatC(j, width = 2, format = "d", flag = "0"),
-                     "_gdas0p5"))
-        }
-      }
-      
-      if (the_month == 6) {
-        jun <- seq(1, 30, 1)
-        for (j in 1:length(jun)) {
-          the_list <- 
-            c(the_list,
-              paste0(the_year, "06",
-                     formatC(j, width = 2, format = "d", flag = "0"),
-                     "_gdas0p5"))
-        }
-      }
-      
-      if (the_month == 7) {
-        jul <- seq(1, 31, 1)
-        for (j in 1:length(jul)) {
-          the_list <- 
-            c(the_list,
-              paste0(the_year, "07",
-                     formatC(j, width = 2, format = "d", flag = "0"),
-                     "_gdas0p5"))
-        }
-      }
-      
-      if (the_month == 8) {
-        aug <- seq(1, 31, 1)
-        for (j in 1:length(aug)) {
-          the_list <- 
-            c(the_list,
-              paste0(the_year, "08",
-                     formatC(j, width = 2, format = "d", flag = "0"),
-                     "_gdas0p5"))
-        }
-      }
-      
-      if (the_month == 9) {
-        sep <- seq(1, 30, 1)
-        for (j in 1:length(sep)) {
-          the_list <- 
-            c(the_list, 
-              paste0(the_year, "09",
-                     formatC(j, width = 2, format = "d", flag = "0"),
-                     "_gdas0p5"))
-        }
-      }
-      
-      if (the_month == 10) {
-        oct <- seq(1, 31, 1)
-        for (j in 1:length(oct)) {
-          the_list <- 
-            c(the_list, 
-              paste0(the_year, "10",
-                     formatC(j, width = 2, format = "d", flag = "0"),
-                     "_gdas0p5"))
-        }
-      }
-      
-      if (the_month == 11) {
-        nov <- seq(1, 30, 1)
-        for (j in 1:length(nov)) {
-          the_list <- 
-            c(the_list,
-              paste0(the_year, "11",
-                     formatC(j, width = 2, format = "d", flag = "0"),
-                     "_gdas0p5"))
-        }
-      }
-      
-      if (the_month == 12) {
-        dec <- seq(1, 31, 1)
-        for (j in 1:length(dec)) {
-          the_list <- 
-            c(the_list, 
-              paste0(the_year, "12",
-                     formatC(j, width = 2, format = "d", flag = "0"),
-                     "_gdas0p5"))
-        }
-      }
+      months <- 
+        formatC(
+          x = months,
+          width = 2,
+          format = "d",
+          flag = "0"
+        )
     }
     
-    # Determine whether all of the requested files are available on the server
-    all_files_available <- ifelse(all(the_list %in% listing), TRUE, FALSE)
+    # Get a vector of the available files
+    files_available <- listing[listing_months %in% months]
     
-    # Determine which files are missing
-    if (all_files_available == FALSE) {
-      for (i in 1:length(the_list)) {
-        if (i == 1) files_missing <- vector(mode = "character", length = 0)
-        if (the_list[i] %in% listing) {
-          NULL
-        } else {
-          files_missing <- c(files_missing, the_list[i])
-        }
-      }
-      
-      # Generate report of missing files
-      missing_report <- 
-        as.data.frame(mat.or.vec(nr = length(files_missing), nc = 2))
-      
-      colnames(missing_report) <- c("filename", "date")
-      
-      # Add names of missing files to the missing file report
-      missing_report$filename <- files_missing
-      
-      # Get dates for missing files
-      for (i in 1:length(files_missing)) {
-        if (i == 1) dates <- mat.or.vec(nr = length(files_missing), nc = 1)
-        
-        dates[i] <- 
-          as.character(ymd(gsub("^([0-9]*).*", "\\1", files_missing[i])))
-      }
-      
-      # Add dates of missing files to the missing file report
-      missing_report$date <- dates
-      
-      # Filter list of files to obtain
-      the_list <- setdiff(the_list, files_missing) 
-    }
-    
-    # Download the requested files from the server
-    for (i in 1:length(the_list)) {
-      
-      download(
-        url = paste0(gdas0p5_dir, the_list[i]),
-        destfile = paste0(path_met_files, the_list[i]),
-        method = "auto",
-        quiet = FALSE,
-        mode = "wb",
-        cacheOK = FALSE
-      )
-    }
-    
-    # Write file that contains report of missing files
-    if (exists("missing_report")) {
-      
-      write.table(
-        missing_report,
-        file = paste0(
-          path_met_files,
-          "Missing GDAS 0.5 Files - ", 
-          format(Sys.time(), "%B %d %Y %H_%M_%S")
-        ),
-        quote = FALSE,
-        row.names = FALS
-      )
-    } 
+    # Download the meteorology data files
+    met_download(
+      urls = file.path(gdas0p5_dir, files_available),
+      local_path = file.path(path_met_files, files_available)
+    )
   }
   
   # Download list of GDAS0p5 met files for one or days
   if (!is.null(days)) {
+  
+    listing_days <- 
+      listing %>%
+      substring(7, 8)
     
-    # Create a list of filenames to compare with those currently available
-    # on the NOAA FTP server
-    for (i in 1:length(days)) {
-      if (i == 1) the_list <- vector(mode = "character", length = 0)
+    if (is.numeric(days)) {
       
-      the_day <- 
-        as.numeric(
-          gsub("^[0-9]*-[0-9][0-9]-([0-9][0-9])", "\\1",
-               days[i]))
-      
-      the_month <- 
-        as.numeric(
-          gsub("^[0-9]*-([0-9][0-9])-.*", "\\1",
-               days[i]))
-      
-      the_year <- 
-        as.numeric(
-          gsub("^([0-9]*).*", "\\1",
-               days[i]))
-      
-      # Determine whether this is a valid date
-      
-      # Construct the filename and add it to 'the_list'
-      the_list <-
-        c(the_list,
-          paste0(
-            the_year,
-            formatC(
-              the_month,
-              width = 2,
-              format = "d",
-              flag = "0"
-            ),
-            formatC(
-              the_day,
-              width = 2,
-              format = "d",
-              flag = "0"
-            ),
-            "_gdas0p5"
-          )
+      days <- 
+        formatC(
+          x = days,
+          width = 2,
+          format = "d",
+          flag = "0"
         )
     }
     
-    # Determine whether all of the requested files are available on the server
-    all_files_available <- ifelse(all(the_list %in% listing), TRUE, FALSE)
+    # Get a vector of the available files
+    files_available <- listing[listing_days %in% days]
     
-    # Determine which files are missing
-    if (all_files_available == FALSE) {
-      for (i in 1:length(the_list)) {
-        
-        if (i == 1) files_missing <- vector(mode = "character", length = 0)
-        
-        if (the_list[i] %in% listing) {
-          NULL
-        } else {
-          files_missing <- c(files_missing, the_list[i])
-        }
-      }
-      
-      # Generate report of missing files
-      missing_report <-
-        as.data.frame(
-          mat.or.vec(nr = length(files_missing), nc = 2)
-        )
-      
-      colnames(missing_report) <- c("filename", "date")
-      
-      # Add names of missing files to the missing file report
-      missing_report$filename <- files_missing
-      
-      # Get dates for missing files
-      for (i in 1:length(files_missing)) {
-        
-        if (i == 1) dates <- mat.or.vec(nr = length(files_missing), nc = 1)
-        dates[i] <- 
-          as.character(ymd(gsub("^([0-9]*).*", "\\1", files_missing[i])))
-      }
-      
-      # Add dates of missing files to the missing file report
-      missing_report$date <- dates
-      
-      # Filter list of files to obtain
-      the_list <- setdiff(the_list, files_missing)
-    }
-    
-    # Download the requested files from the server
-    for (i in 1:length(the_list)) {
-      download(
-        url = paste0(gdas0p5_dir, the_list[i]),
-        destfile = paste0(path_met_files, the_list[i]),
-        method = "auto",
-        quiet = FALSE,
-        mode = "wb",
-        cacheOK = FALSE
-      )
-    }
-    
-    # Write file that contains report of missing files
-    if (exists("missing_report")) {
-      
-      write.table(
-        missing_report,
-        file = paste0(
-          path_met_files,
-          "Missing GDAS 0.5 Files - ", 
-          format(Sys.time(), "%B %d %Y %H_%M_%S")
-        ),
-        quote = FALSE,
-        row.names = FALSE
-      )
-    }
+    # Download the meteorology data files
+    met_download(
+      urls = file.path(gdas0p5_dir, files_available),
+      local_path = file.path(path_met_files, files_available)
+    )  
   }
+  
 }
