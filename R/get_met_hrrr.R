2b4b6dc35050ad52b28e545bd3eb2c5e082b6910
Richard Iannone <riannone@me.com>
2019-08-05 21:55:13 -0400

    Remove unneeded arguments

--- R/get_met_hrrr.R
+++ R/get_met_hrrr.R
@@ -4,14 +4,14 @@
 #' files from the NOAA FTP server and saves them to a specified folder. Files
 #' can be downloaded only by specifying a list of filenames (in the form of
 #' `'RP[YYYY][MM].gbl'`).
+#' 
 #' @param files a vector list of exact filenames for the HRRR files.
 #' @param path_met_files a full path should be provided for the location of the
 #'   meteorological data files; downloaded files will be saved in this location.
 #' @export
 get_met_hrrr <- function(files = NULL,
-                          years = NULL,
-                          months = NULL,
-                          path_met_files) {
+                         path_met_files) {
+                           
   ftp_dir <- "ftp://arlftp.arlhq.noaa.gov/pub/archives/hrrr/"
   
   # Download list of reanalysis met files by name
