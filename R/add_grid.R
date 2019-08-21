2de00fece00d0107491655782c4567a1ad788856
Richard Iannone <riannone@me.com>
2019-08-20 23:28:35 -0400

    Rewrite the `add_grid()` function

--- R/add_grid.R
+++ R/add_grid.R
@@ -47,7 +47,7 @@ add_grid <- function(model,
                      samp_interval = 24,
                      name = NULL) {
   
-  if (inherits(model, "traj_model")) {
+  if (inherits(model, "trajectory_model")) {
     
     # Obtain the grid of lat/lon points
     grid <- 
@@ -61,96 +61,6 @@ add_grid <- function(model,
     # Add the grid points to the model object
     model$lat <- grid$lat
     model$lon <- grid$lon
-    
-    return(model)
-  }
-  
-  if (inherits(model, "disp_model")) {
-    
-    if (is.null(name)) {
-      if (is.null(model$grids)) {
-        name <- "grid_1"
-      } else {
-        name <- paste0("grid_", nrow(model$grids) + 1)
-      }
-    }
-    
-    if (is.null(lat)) {
-      if (is.null(model$lat)) {
-        lat <- NA  
-      } else {
-        lat <- model$lat
-      }
-    }
-    
-    if (is.null(lon)) {
-      if (is.null(model$lon)) {
-        lon <- NA  
-      } else {
-        lon <- model$lon
-      }
-    }
-    
-    if (is.null(heights)) {
-      heights <- 50
-      layers <- 1
-    } else {
-      layers <- length(heights)
-      heights <- paste(heights, collapse = " ")
-    }
-    
-    if (is.null(start_day)) {
-      if (!is.null(model$start_day)) {
-        start_day <- model$start_day
-      } else {
-        start_day <- NA
-      }
-    }
-    
-    if (is.null(start_hour)) {
-      if (!is.null(model$start_hour)) {
-        start_hour <- model$start_hour
-      } else {
-        start_hour <- NA
-      }
-    }
-    
-    if (is.null(end_day) & 
-        is.null(end_hour)) {
-      
-      duration <- NA
-      end_day <- NA
-      end_hour <- NA
-    }
-    
-    # Write grid parameters to a data frame
-    grid <- 
-      data.frame(
-        name = name,
-        lat = lat,
-        lon = lon,
-        range_lat = range[1],
-        range_lon = range[2],
-        division_lat = division[1],
-        division_lon = division[2],
-        duration = duration,
-        start_day = start_day,
-        start_hour = start_hour,
-        end_day = end_day,
-        end_hour = end_hour,
-        heights = heights,
-        samp_type = samp_type,
-        samp_interval = samp_interval,
-        stringsAsFactors = FALSE
-      )
-    
-    # Write data frame to the `grids` list
-    # component of `model`
-    if (is.null(model$grids)) {
-      model$grids <- grid
-    } else {
-      model$grids <- rbind(model$grids, grid)
-    }
     
     return(model)
   }
