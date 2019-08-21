d548b626ae7feae911e6c848f9f97949db282b4d
Richard Iannone <riannone@me.com>
2019-08-20 23:38:25 -0400

    Rename function/file

---
+++ R/create_dispersion_model.R
@@ -0,0 +1,28 @@
+#' Create a dispersion model
+#'
+#' Create a dispersion model object to begin a modeling pipeline.
+#' 
+#' @export
+create_dispersion_model <- function() {
+  
+  # Create the 'disp_model' list object
+  disp_model <- 
+    list(
+      # disp_name = NULL,
+      # lat = NULL,
+      # lon = NULL,
+      # height = NULL,
+      start_time = NULL,
+      end_time = NULL,
+      direction = "forward",
+      met_type = NULL,
+      sources = NULL,
+      vert_motion = 0,
+      model_height = 20000,
+      disp_df = NULL
+    )
+  
+  class(disp_model) <- "dispersion_model"
+  
+  disp_model
+}
