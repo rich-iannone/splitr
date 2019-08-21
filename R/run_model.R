5a24546be64c458559e3c6117d4fd90521c74ca5
Richard Iannone <riannone@me.com>
2019-08-20 23:36:46 -0400

    Rewrite the `run_model()` fcn

--- R/run_model.R
+++ R/run_model.R
@@ -7,8 +7,8 @@
 #' @export
 run_model <- function(model) {
   
-  if (inherits(model, "traj_model")) {
-
+  if (inherits(model, "trajectory_model")) {
+    
     traj_df <- 
       hysplit_trajectory(
         lat = model$lat,
@@ -37,37 +37,54 @@ run_model <- function(model) {
     return(model)
   }
   
-  if (inherits(model, "disp_model")) {
+  if (inherits(model, "dispersion_model")) {
     
-    disp_df <- 
-      hysplit_dispersion(
-        lat = model$lat,
-        lon = model$lon,
-        height = ifelse(is.null(model$height),
-                        50, model$height),
-        duration = ifelse(is.null(model$duration),
-                          24, model$duration),
-        start_day = model$start_day,
-        start_hour = model$start_hour,
-        direction = ifelse(is.null(model$direction),
-                           "forward", model$direction),
-        met_type = ifelse(is.null(model$met_type),
-                          "reanalysis", model$met_type),
-        vert_motion = ifelse(is.null(model$vert_motion),
-                             0, model$vert_motion),
-        model_height = ifelse(is.null(model$model_height),
-                              20000, model$model_height),
-        particle_num = 2500,
-        particle_max = 10000,
-        emissions = model$emissions,
-        species = model$species,
-        grids = model$grids,
-        exec_dir = model$exec_dir,
-        met_dir = model$met_dir,
-        binary_path = model$binary_path
-      )
-   
-    model$disp_df <- disp_df
+    n_model_runs <- seq(nrow(model$sources))
+    
+    for (i in n_model_runs) {
+      
+      # Get time window for observations
+      start_day <- model$start_time %>% lubridate::floor_date()
+      start_hour <- model$start_time %>% lubridate::hour() 
+      duration <- as.numeric(model$end_time - model$start_time)
+      
+      # Get ith source parameters
+      lat <- model$sources[i, ][["lat"]]
+      lon <- model$sources[i, ][["lon"]]
+      height <- model$sources[i, ][["height"]]
+      
+      release_start <- model$sources[i, ][["release_start"]]
+      release_end <- model$sources[i, ][["release_end"]]
+      
+      rate <- model$sources[i, ][["rate"]]
+      
+      species_list <- 
+        model$sources[i, ] %>% 
+        dplyr::select(-c(lat, lon, height)) %>%
+        as.list()
+      
+      disp_df <- 
+        hysplit_dispersion(
+          lat = lat,
+          lon = lon,
+          height = height,
+          start_day = start_day,
+          start_hour = start_hour,
+          duration = duration,
+          direction = "forward",
+          met_type = model$met_type,
+          vert_motion = model$vert_motion,
+          model_height = model$model_height,
+          particle_num = 2500,
+          particle_max = 10000,
+          species = species_list,
+          exec_dir = model$exec_dir,
+          met_dir = model$met_dir,
+          binary_path = model$binary_path
+        )
+      
+      model$disp_df <- disp_df
+    }
     
     return(model) 
   }
