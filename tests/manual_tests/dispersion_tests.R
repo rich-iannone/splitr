c6f036c8227e3bf5205baad29252418eb39deed5
Richard Iannone <riannone@me.com>
2019-08-21 00:00:47 -0400

    Update manual tests for dispersion models

--- tests/manual_tests/dispersion_tests.R
+++ tests/manual_tests/dispersion_tests.R
@@ -3,35 +3,22 @@ library(tidyverse)
 library(here)
 
 # Dispersion tests
+
 dispersion_model <-
-  create_disp_model() %>%
-  add_emissions(
-    rate = 5,
-    duration = 6,
-    start_day = "2015-07-01",
-    start_hour = 0
+  create_dispersion_model() %>%
+  add_source(
+    name = "particle",
+    lat = 49.0, lon = -123.0, height = 50,
+    pdiam = 15, density = 1.5, shape_factor = 0.8,
+    release_start = lubridate::ymd_hm("2015-07-01 00:00"),
+    release_end = lubridate::ymd_hm("2015-07-01 00:00") + lubridate::hours(2)
   ) %>%
-  add_species(
-    pdiam = 1,
-    density = 1,
-    shape_factor = 1
-  ) %>%
-  add_grid(
-    lat = 43.45,
-    lon = -79.70,
-    height = 50,
-    range = c(0.5, 0.5),
-    division = c(0.1, 0.1)
-  ) %>%
-  add_params(
-    duration = 24,
-    start_day = "2015-07-01",
-    start_hour = 0,
-    direction = "forward",
+  add_dispersion_params(
+    start_time = lubridate::ymd_hm("2015-07-01 00:00"),
+    end_time = lubridate::ymd_hm("2015-07-01 00:00") + lubridate::hours(6),
+    direction = "forward", 
     met_type = "reanalysis",
     met_dir = here::here("met"),
     exec_dir = here::here("out")
   ) %>%
   run_model()
-
-
