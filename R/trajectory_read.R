aebbb5f194de5821bce293a6e202ea1b40d69ce9
Richard Iannone <riannone@me.com>
2019-09-19 17:08:23 -0400

    Rewrite the `trajectory_read()` fcn

--- R/trajectory_read.R
+++ R/trajectory_read.R
@@ -20,159 +20,163 @@ trajectory_read <- function(output_folder) {
   trajectory_file_list <- 
     list.files(
       path = output_folder,
-      pattern = "^traj.*"
+      pattern = "^traj-.*"
     )
   
   # Initialize empty tibble with 12 columns
-  traj_df <-
+  traj_tbl <-
     dplyr::tibble(
       receptor = integer(0),
       year = integer(0),
       month = integer(0),
       day = integer(0),
       hour = integer(0),
-      hour.inc = integer(0),
+      hour_along = integer(0),
       lat = numeric(0),
       lon = numeric(0), 
       height = numeric(0),
       pressure = numeric(0),
-      date2 = lubridate::as_datetime("2015-01-01")[-1],
-      date = lubridate::as_datetime("2015-01-01")[-1]
+      traj_dt = lubridate::as_datetime("2015-01-01")[-1],
+      traj_dt_i = lubridate::as_datetime("2015-01-01")[-1]
+    )
+  
+  extended_col_names <- 
+    c(
+      "year", "month", "day", "hour", "hour_along",
+      "lat", "lon", "height", "pressure",
+      "theta", "air_temp", "rainfall", "mixdepth", "rh", "sp_humidity", 
+      "h2o_mixrate", "terr_msl", "sun_flux"
+    )
+  
+  standard_col_names <- 
+    c(
+      "year", "month", "day", "hour", "hour_along",
+      "lat", "lon", "height", "pressure"
     )
   
   # Process all trajectory files
   for (file_i in trajectory_file_list) {
     
-    # For each trajectory file, read each line and
-    # determine where the variable-length header ends
-    column_widths <- 92
-    
     file_i_path <- file.path(output_folder, file_i)
-    
-    file_lines <- readLines(file_i_path)
-    widths <- nchar(file_lines)
-    
-    if (any(widths == 18)) {
-      
-      read_characters <- readChar(file_i_path, file.info(file_i_path)$size)
-      
-      cat(gsub("([-0-9\\. ]{19}[-0-9\\. ])\n",
-               "\\1", read_characters),
-          file = file_i_path,
-          sep = "\n")
-    }
-    
-    traj_temp <- 
-      try(utils::read.fwf(file = file_i_path, widths = column_widths))
-    
-    if (class(traj_temp) != "try-error") {
-      
-      for (j in seq(nrow(traj_temp))) {
-        
-        if (length(grep("PRESSURE", traj_temp[j, 1])) != 0) {
-          skip_up_to_line <- j
-        } 
-      }
-      
-      column_widths <- c(rep(6, 8), 8, rep(9, 4))
-      
-      traj <- try(
-        utils::read.fwf(
-          file = file_i_path,
-          skip = skip_up_to_line,
-          widths = column_widths
-        )
-      )
-      
-      if (class(traj) != "try-error") {
 
-        traj <-
-          traj %>%
-          dplyr::as_tibble() %>%
-          .[, -c(1, 7:8)] %>%
-          tidyr::drop_na()
-          
-        names(traj) <-
-          c("receptor", "year", "month", "day", "hour", "hour.inc",
-            "lat", "lon", "height", "pressure")
-        
-        traj <-
-          traj %>%
-          dplyr::mutate(hour.inc = as.integer(hour.inc)) %>%
-          dplyr::mutate(year_full = ifelse(year < 50, year + 2000, year + 1900)) %>%
-          tidyr::unite(col = date_str, year_full, month, day, sep = "-", remove = FALSE) %>%
-          tidyr::unite(col = date_h_str, date_str, hour, sep = " ", remove = FALSE) %>%
-          dplyr::mutate(date2 = lubridate::ymd_h(date_h_str)) %>%
-          dplyr::select(-c(date_h_str, date_str, year_full)) %>%
-          dplyr::mutate(date = date2[1])
-        
-        # Continuously bind data frames together to make
-        # a large df from all trajectory files
-        traj_df <- traj_df %>% dplyr::bind_rows(traj)
-      }
-    }
-  }
- 
-  widths_1 <-
-    readLines(con = file.path(output_folder, trajectory_file_list[1])) %>%
-    nchar()
-
-  if (any(widths_1 == 173)) {
+    file_lines <- readLines(file_i_path, encoding = "UTF-8", skipNul = TRUE)
     
-    # Initialize empty data frame with 14 named columns
-    traj_extra_df <-
-      dplyr::tibble(
-        receptor = integer(0),
-        year = integer(0),
-        month = integer(0),
-        day = integer(0),
-        hour = integer(0),
-        theta = numeric(0),
-        air_temp = numeric(0), 
-        rainfall = numeric(0),
-        mixdepth = numeric(0),
-        rh = numeric(0),
-        sp_humidity = numeric(0),
-        h2o_mixrate = numeric(0),
-        terr_msl = numeric(0),
-        sun_flux = numeric(0)
-      )
+    file_one_line <- readr::read_file(file_i_path)
     
-    extra_column_widths <- c(rep(6, 8), 8, rep(9, 13))
+    header_line <- 
+      file_lines %>%
+      vapply(
+        FUN.VALUE = logical(1),
+        USE.NAMES = FALSE,
+        function(x) tidy_grepl(x, "PRESSURE")
+      ) %>%
+      which()
     
-    for (file_i in trajectory_file_list) {
+    file_lines_data <- 
+      file_lines[(header_line + 1):(length(file_lines))] %>%
+      tidy_gsub("\\s\\s*", " ") %>%
+      tidy_gsub("^ ", "")
+    
+    if (!file_one_line %>% tidy_grepl("AIR_TEMP")) {
       
-      file_i_path <- file.path(output_folder, file_i)
-
-      traj_extra <- 
-        utils::read.fwf(
-          file = file_i_path,
-          skip = skip_up_to_line,
-          widths = extra_column_widths
+      #
+      # Standard meteorology
+      #
+      
+      traj_tbl_i <- 
+        file_lines_data %>%
+        strsplit("\\s+") %>%
+        lapply(
+          FUN = function(x) {
+            x[c(3:6, 9:13)] %>%
+              as.numeric() %>%
+              stats::setNames(standard_col_names) %>%
+              as.list() %>%
+              dplyr::as_tibble()
+          }
         ) %>%
-        dplyr::as_tibble() %>%
-        .[, c(2:6, 14:22)] %>%
-        tidyr::drop_na()
+        dplyr::bind_rows() %>%
+        dplyr::mutate_at(
+          .vars = dplyr::vars(year, month, day, hour, hour_along), 
+          .funs = as.integer
+        ) %>%
+        dplyr::mutate(year_full = ifelse(year < 50, year + 2000, year + 1900)) %>%
+        tidyr::unite(col = date_str, year_full, month, day, sep = "-", remove = FALSE) %>%
+        tidyr::unite(col = date_h_str, date_str, hour, sep = " ", remove = FALSE) %>%
+        dplyr::mutate(traj_dt = lubridate::ymd_h(date_h_str)) %>%
+        dplyr::select(-c(date_h_str, date_str, year_full)) %>%
+        dplyr::mutate(traj_dt_i = traj_dt[1])
       
-      names(traj_extra) <- 
-        c("receptor", "year", "month", "day", "hour",
-          "theta", "air_temp", "rainfall", "mixdepth", "rh", "sp_humidity", 
-          "h2o_mixrate", "terr_msl", "sun_flux")
-      
-      # Continuously bind data frames together to make
-      # a large df from all trajectory files
-      traj_extra_df <- traj_extra_df %>% dplyr::bind_rows(traj_extra)
+      traj_tbl <- traj_tbl %>% dplyr::bind_rows(traj_tbl_i)
     }
-
-    #traj_df <- traj_df %>% dplyr::bind_cols(traj_extra_df)
     
-    traj_df <-
-      traj_df %>% 
-      dplyr::left_join(
-        traj_extra_df,
-        by = c("receptor", "year", "month", "day", "hour")
-      )
+    if (file_one_line %>% tidy_grepl("AIR_TEMP")) {
+      
+      #
+      # Extended meteorology
+      #
+      
+      file_lines_data_20 <- 
+        file_lines_data %>%
+        vapply(
+          FUN.VALUE = logical(1),
+          USE.NAMES = FALSE,
+          function(x) {
+            tidy_grepl(
+              x, 
+              paste0(
+                "^",
+                rep("[0-9\\.-]*?", 20) %>% paste(collapse = " "),
+                "$"
+              )
+            )
+          }
+        )
+      
+      file_lines_data_02 <- 
+        file_lines_data %>%
+        vapply(
+          FUN.VALUE = logical(1),
+          USE.NAMES = FALSE,
+          function(x) {
+            tidy_grepl(
+              x, 
+              paste0(
+                "^",
+                rep("[0-9\\.-]*?", 2) %>% paste(collapse = " "),
+                "$"
+              )
+            )
+          }
+        )
+      
+      traj_tbl_i <- 
+        paste(file_lines_data[file_lines_data_20], file_lines_data[file_lines_data_02]) %>%
+        strsplit("\\s+") %>%
+        lapply(
+          FUN = function(x) {
+            x[c(3:6, 9:22)] %>%
+              as.numeric() %>%
+              stats::setNames(extended_col_names) %>%
+              as.list() %>%
+              dplyr::as_tibble()
+          }
+        ) %>%
+        dplyr::bind_rows() %>%
+        dplyr::mutate_at(
+          .vars = dplyr::vars(year, month, day, hour, hour_along), 
+          .funs = as.integer
+        ) %>%
+        dplyr::mutate(year_full = ifelse(year < 50, year + 2000, year + 1900)) %>%
+        tidyr::unite(col = date_str, year_full, month, day, sep = "-", remove = FALSE) %>%
+        tidyr::unite(col = date_h_str, date_str, hour, sep = " ", remove = FALSE) %>%
+        dplyr::mutate(traj_dt = lubridate::ymd_h(date_h_str)) %>%
+        dplyr::select(-c(date_h_str, date_str, year_full)) %>%
+        dplyr::mutate(traj_dt_i = traj_dt[1])
+      
+      traj_tbl <- traj_tbl %>% dplyr::bind_rows(traj_tbl_i)
+    }
   }
   
-  traj_df
+  traj_tbl
 }
