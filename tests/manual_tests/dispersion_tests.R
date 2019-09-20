4c509abb32a9904f8944fb476c702b6bcf545e81
Richard Iannone <riannone@me.com>
2019-09-20 16:06:24 -0400

    Update dispersion_tests.R

--- tests/manual_tests/dispersion_tests.R
+++ tests/manual_tests/dispersion_tests.R
@@ -10,12 +10,15 @@ dispersion_model <-
     name = "particle",
     lat = 49.0, lon = -123.0, height = 50,
     pdiam = 15, density = 1.5, shape_factor = 0.8,
+    rate = 10,
     release_start = lubridate::ymd_hm("2015-07-01 00:00"),
-    release_end = lubridate::ymd_hm("2015-07-01 00:00") + lubridate::hours(2)
+    release_end = lubridate::ymd_hm("2015-07-01 00:00") + 
+      lubridate::hours(2)
   ) %>%
   add_dispersion_params(
     start_time = lubridate::ymd_hm("2015-07-01 00:00"),
-    end_time = lubridate::ymd_hm("2015-07-01 00:00") + lubridate::hours(6),
+    end_time = lubridate::ymd_hm("2015-07-01 00:00") + 
+      lubridate::hours(6),
     direction = "forward", 
     met_type = "reanalysis",
     met_dir = here::here("met"),
